ef94b2432f1b555434784fa2a4503a1622481c5c
Sahani19020821 <sahani19020821@gmail.com>
2021-09-12 14:20:51 +0530

    Put 'body' instead of '*' in reset-password.css (line 3). Added parent0child indentation in reset-password.php. unwanted spaces in reset-password.php were removed. Included the header file. Added the Logo.

--- pages/reset-password.php
+++ pages/reset-password.php
@@ -1,40 +1,32 @@
-<script
-        src="https://kit.fontawesome.com/64d58efce2.js"
-        crossorigin="anonymous"
-></script>
+<?php include '../components/header.php';?>
 <link rel="stylesheet" href="../assets/styles/reset-password.css" />
 
 <div class='reset-password'>
-
-    <h2 class='title'>Reset Password</h2>
+    <div class=img>
+        <img src='../assets/images/logo.jpeg' alt='logo' width='150' height='150'> <br>
+    </div>
+    <p class='title'>Reset Password</p>
     <form id='form' action="#" method="POST" class='reset-password-form'>
-<!--        <img src='Reset_Password_images/logo.jpeg' alt='logo' width='150' height='150'> <br>-->
-
         <div class='container1'>
-        <div class='labels'>
+          <div class='labels'>
             <label>Password</label>
+          </div>
+          <div class='input-field'>
+            <input id='password' type="password" placeholder="Enter Password" />
+          </div>
         </div>
-
-        <div class='input-field'>
-            <input id='password' type="password"  />
-        </div>
-        </div>
-
         <div class='container2'>
-        <div class='labels'>
+          <div class='labels'>
             <label>Confirm Password</label>
         </div>
-
-        <div class='input-field'>
-            <input id='confirmPassword' type="password" />
-        </div>
+          <div class='input-field'>
+            <input id='confirmPassword' type="password" placeholder="Re-enter Password"/>
+          </div>
         </div>
         <div id='error'></div>
-
-        <input type='submit' value="Submit" class='btn-solid'> &nbsp; &nbsp;
-
+        <input type='submit' value="Submit" class='btn-solid'>
     </form>
-
 </div>
 
-      <script src="../js/reset-password.js"> </script>
+<script src="../js/reset-password.js"> </script>
+<?php include '../components/footer.php';?>
