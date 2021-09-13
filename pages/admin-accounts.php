6a09ea39e66edec6799ea6c4958a417bcfd5bab9
IsuruVihan <isuruvihan@gmail.com>
2021-09-14 01:51:30 +0530

    UI/UX of Container-1 and Container-2 in Details section done

--- pages/admin-accounts.php
+++ pages/admin-accounts.php
@@ -312,10 +312,52 @@
         </div>
         <div class='row-1'>
             <div class='container-1'>
-                Container-1
+                <div class='section-1'>
+                    <img src='../assets/images/user-default.png' width='100%' class='user-pic' alt='user-pic' />
+                    <div class='account-type'>
+                        Account Type
+                    </div>
+                </div>
+                <div class='section-2'>
+                    <div class='subscription-type'>
+                        Subscription Type
+                    </div>
+                    <div class='due-date'>
+                        Due Date
+                    </div>
+                    <button class='recharge-report-btn btn'>
+                        Recharge Report
+                    </button>
+                </div>
+                <div class='section-3'>
+                    <div class='sec-row-1'>
+                        <button class='accept-btn btn'>Accept</button>
+                        <button class='remove-btn btn'>Remove</button>
+                    </div>
+                    <div class='sec-row-2'>
+                        <button class='ban-btn btn'>Ban</button>
+                        <button class='unban-btn btn'>Unban</button>
+                    </div>
+                </div>
             </div>
             <div class='container-2'>
-                Container-2
+                <div class='full-name details-field'>Full Name</div>
+                <div class='middle-section'>
+                    <div class='mid-sec-row'>
+                        <div class='first-name details-field'>First Name</div>
+                        <div class='last-name details-field'>Last Name</div>
+                    </div>
+                    <div class='mid-sec-row'>
+                        <div class='gender details-field'>Gender</div>
+                        <div class='batch details-field'>Batch</div>
+                    </div>
+                    <div class='mid-sec-row'>
+                        <div class='nic details-field'>NIC</div>
+                        <div class='contact-number details-field'>Contact Number</div>
+                    </div>
+                </div>
+                <div class='email details-field'>Email</div>
+                <div class='address details-field'>Address</div>
             </div>
         </div>
         <div class='row-2'>
