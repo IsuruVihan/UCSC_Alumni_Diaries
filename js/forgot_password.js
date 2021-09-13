8538c6055fd1c93b0465434d5acd5a15981378a8
pinsaradhanika97 <pinsaradhanika@gmail.com>
2021-09-13 19:57:01 +0530

    changes made accordingly

--- js/forgot_password.js
+++ js/forgot_password.js
@@ -9,37 +9,6 @@ if (x.innerHTML === "Step 1 : Enter your email address") {
     
 }
 
-
-/*function errorMessage() {
-
-    let error = document.getElementById("error");
-    if (isNaN(document.getElementById("otp").value)) {
-    error.textContent = "Please enter a valid number";
-    error.style.color = "red";
-    }
-
-    else 
-    {error.textContent = ""; }
-   }
-*/
-
-/*function ValidateEmail(inputText){
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(inputText.value.match(mailformat))
-    {
-        alert("Valid email address!");
-        document.form-mail.email.focus();
-        return true;
-    }
-    else{
-        alert("You have entered an invalid email address!");
-        document.form-mail.email.focus();
-        return false;
-        }
-}
-*/
-
-
 function checkEmail() {
 
     var email = document.getElementById('email');
--- pages/forgot_password.php
+++ pages/forgot_password.php
@@ -9,30 +9,28 @@
         <script src="../js/forgot_password.js"></script>
 
           
-            <div id='myDIV'>Step 1 : Enter your email address</div>   
-                    <!-- OTP request container-->
-            <section class='container-wrapper'>
-                                   
-                <img src="../assets/images/alumniDiaries_logo_transparent" alt="alumni diaries logo" class="alumniDiaries_logo">
+        <div id='myDIV'>Step 1 : Enter your email address</div>   
+<!-- OTP request container-->
+        <section class='container-wrapper'>                                
 
-                <header>Forgot Your Password?</header>
-                        
-                 <form action='' method='POST' id='container-1' name='form-mail'>
-                 <label for='email' class='labeling'> Email Address   
-                        <input type='text' name='email' id='email' placeholder='Enter your Email address' class='input-style' required>
-                 </label>
-                    <input type='submit' value='Send OTP' class='button1' onsubmit='myFunction()' onclick='checkEmail();container_2Show();container_1Hide();' >           
-                 </form>
+                <header>Forgot Password</header>
 
-              <form action='#' method='POST' id='container-2'>
-              <label for='otp' class='labeling'>OTP     
-                <input type='text' id='otp' placeholder='OTP Code' class='input-style' required maxlength='6'>
-              </label> 
-              <span id='error'> </span>
-                    <input type='submit' value='Submit' class='button1' >
-              </form>
-                        <a href='login.php' class='return_login'>Return to login</a>
-             </section>                         
+                <form action='' method='POST' id='container-1' name='form-mail'>
+                        <label for='email' class='labeling'> Email Address   
+                                <input type='text' name='email' id='email' placeholder='Enter your Email address' class='input-style' required>
+                        </label>
+                        <input type='submit' value='Send OTP' class='button1' onsubmit='myFunction()' onclick='checkEmail();container_2Show();container_1Hide();' >           
+                </form>
+
+                <form action='#' method='POST' id='container-2'>
+                        <label for='otp' class='labeling'>OTP     
+                                <input type='text' id='otp' placeholder='OTP Code' class='input-style' required maxlength='6'>
+                        </label> 
+                        <span id='error'> </span>
+                        <input type='submit' value='Submit' class='button1' >
+                </form>
+                <a href='login.php' class='return_login'>Return to login</a>
+        </section>                         
 </div>
 
 <?php include('../components/footer.php'); ?>
