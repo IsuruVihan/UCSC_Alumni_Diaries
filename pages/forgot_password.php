91f1de66bd611b32121f3ee331b47d115cd7eed4
pinsaradhanika97 <pinsaradhanika@gmail.com>
2021-09-14 09:22:45 +0530

    fixed the alignment issues

--- pages/forgot_password.php
+++ pages/forgot_password.php
@@ -13,15 +13,14 @@
                 <form action='' method='POST' id='container-1' name='form-mail'>
                         <label for='email' class='labeling'> Email Address   
                                 <input type='text' name='email' id='email' placeholder='Enter your Email address' class='input-style' required>
-                        </label>
-                        <input type='submit' value='Send OTP' class='button1' onsubmit='myFunction()' onclick='checkEmail();container_2Show();container_1Hide();' >           
+                                <input type='submit' value='Send OTP' id='button1' onsubmit='myFunction()' onclick='checkEmail();container_2Show();container_1Hide();' >           
+                        </label> 
                 </form>
                 <form action='#' method='POST' id='container-2'>
                         <label for='otp' class='labeling'>OTP     
                                 <input type='text' id='otp' placeholder='OTP Code' class='input-style' required maxlength='6'>
+                                <input type='submit' value='Submit' id='button1' >
                         </label> 
-                        <span id='error'> </span>
-                        <input type='submit' value='Submit' class='button1' >
                 </form>
                 <a href='login.php' class='return_login'>Return to login</a>
         </section>                         
