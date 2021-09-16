069f2e1eb5e2ba25bddb5f5d2c7d7239c2a0156a
IsuruVihan <isuruvihan@gmail.com>
2021-09-16 10:41:25 +0530

    Projects nav page UI/UX done

--- pages/projects.php
+++ pages/projects.php
@@ -12,7 +12,66 @@
     </p>
 </div>
 <div class='projects'>
-    Projects
+    <a class='card-link' href='./admin-accounts.php'>
+        <div class='card' id='accounts-card'>
+            <p class='title'>
+                Not Started Yet
+            </p>
+            <div class='img-container'>
+                <img src='../assets/gifs/not_started_yet_project.gif' alt='not-started-yet' width='100%' />
+            </div>
+            <div class='description' id='accounts-description'>
+                <ul class='list'>
+                    <li>About to start</li>
+                </ul>
+            </div>
+        </div>
+    </a>
+    <a class='card-link' href='./admin-reports.php'>
+        <div class='card' id='reports-card'>
+            <p class='title'>
+                Ongoing
+            </p>
+            <div class='img-container'>
+                <img src='../assets/gifs/ongoing_project.gif' alt='ongoing' width='100%' />
+            </div>
+            <div class='description' id='reports-description'>
+                <ul class='list'>
+                    <li>Currently in progress</li>
+                </ul>
+            </div>
+        </div>
+    </a>
+    <a class='card-link' href='./admin-subscriptions.php'>
+        <div class='card' id='subscriptions-card'>
+            <p class='title'>
+                Closed
+            </p>
+            <div class='img-container'>
+                <img src='../assets/gifs/closed_project.gif' alt='closed' width='100%' />
+            </div>
+            <div class='description' id='subscriptions-description'>
+                <ul class='list'>
+                    <li>Closed without completing</li>
+                </ul>
+            </div>
+        </div>
+    </a>
+    <a class='card-link' href='./admin-project-spendings.php'>
+        <div class='card' id='spendings-card'>
+            <p class='title'>
+                Completed
+            </p>
+            <div class='img-container'>
+                <img src='../assets/gifs/completed_project.gif' alt='completed' width='100%' />
+            </div>
+            <div class='description' id='spendings-description'>
+                <ul class='list'>
+                    <li>Successfully completed</li>
+                </ul>
+            </div>
+        </div>
+    </a>
 </div>
 
 <?php include('../components/footer.php'); ?>
