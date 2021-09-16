144abcd34ffef061a049b694fd3c6786031339e4
jayaweera2 <subhagyajayaweera1998@gmail.com>
2021-09-16 19:13:39 +0530

    save changes

--- pages/group-chat.php
+++ pages/group-chat.php
@@ -15,22 +15,43 @@
 </div>
 <div class='group-chat'>
    <div class='chat-list'>
+       <div class='card chat-list'>
    <div class='title'>
             Chat List
         </div>
        <div class='filter'>
            <div class='col01'>
-                 <input class='input-field' type='text' placeholder=' Name'/>
-                 <input class='input-field' type='text' placeholder=' Name'/>
-
-            
-                <div class='col2'>
-                      <button class='filter-btn btn'>Filter</button>
-                </div>  
-
+                 <input class='input-field' type='text' placeholder='First Name'/>
+                 <input class='input-field' type='text' placeholder=' Last Name'/>
+                   <select class='input-field'>
+                       <option value='batch'>Batch</option>
+                       <option value='2004-2005'>2004-2005</option>
+                       <option value='2005-2006'>2005-2006</option>
+                       <option value='2006-2007'>2006-2007</option>
+                       <option value='2007-2008'>2007-2008</option>
+                       <option value='2008-2009'>2008-2009</option>
+                       <option value='2009-2010'>2009-2010</option>
+                       <option value='2010-2011'>2010-2011</option>
+                       <option value='2011-2012'>2011-2012</option>
+                       <option value='2012-2013'>2012-2013</option>
+                       <option value='2013-2014'>2013-2014</option>
+                       <option value='2014-2015'>2014-2015</option>
+                       <option value='2015-2016'>2015-2016</option>
+                       <option value='2016-2017'>2016-2017</option>
+                       <option value='2017-2018'>2017-2018</option>
+                       <option value='2018-2019'>2018-2019</option>
+                       <option value='2019-2020'>2019-2020</option>
+                       <option value='2020-2021'>2020-2021</option>
+                       <option value='2021-2022'>2021-2022</option>
+                       <option value='2022-2023'>2022-2023</option>
+                      </select>
             </div>
-     
-       </div>
+            <div class='col02'>
+                <button class='filter-btn btn'>Filter</button>
+            </div>
+        </div>       
+
+</div>
 </div>
        <div class='chat-wall'>
 
