329b14daf812da29c241a1376914cf5b78911bd9
Sahani19020821 <sahani19020821@gmail.com>
2021-09-16 11:36:18 +0530

    Private Chat full sketch done

---
+++ pages/private-chat.php
@@ -0,0 +1,24 @@
+<?php include('../components/header.php'); ?>
+<link rel='stylesheet' href='../assets/styles/private-chat.css'/>
+
+<div class='main-container'
+    <p class='breadcrumb'>
+        <a href='home.php'>Home</a> / Private Chat
+    </p>
+    <p class='main-title'>
+         Private Chat
+    </p>
+</div>
+<div class='private-chat'>
+    <div class='chat-list'>
+
+    </div>
+    <div class='chat'>
+
+    </div>
+    <div class='available-users'>
+
+    </div>
+</div>
+
+<?php include('../components/footer.php'); ?>
