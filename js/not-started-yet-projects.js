fde3e654b3d20b9bb0c983710b5f729fc8ac0476
IsuruVihan <isuruvihan@gmail.com>
2021-09-16 15:21:02 +0530

    Not started yet projects UI/UX done

---
+++ js/not-started-yet-projects.js
@@ -0,0 +1,9 @@
+const DisplayButtons = (id) => {
+    const element = document.getElementById(id);
+    element.style.display = "block";
+}
+
+const HideButtons = (id) => {
+    const element = document.getElementById(id);
+    element.style.display = "none";
+}
--- pages/not-started-yet-projects.php
+++ pages/not-started-yet-projects.php
@@ -1,6 +1,7 @@
 <link rel='stylesheet' href='../assets/styles/not_started_yet_projects.css'/>
 <link rel='stylesheet' href='https://pro.fontawesome.com/releases/v5.10.0/css/all.css'
       integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p' crossorigin='anonymous'/>
+<script src='../js/not-started-yet-projects.js'></script>
 
 <?php include('../components/header.php'); ?>
 
@@ -14,14 +15,92 @@
     </p>
 </div>
 <div class='not-started-yet-projects'>
-    <div class='projects-list'>
-    
+    <div class='card projects-list'>
+        <div class='title'>
+            Related Projects
+        </div>
+        <div class='filter'>
+            <div class='col1'>
+                <input class='input-field date-field' type='date' placeholder='First Name'/>
+                to
+                <input class='input-field date-field' type='date' placeholder='Last Name'/>
+            </div>
+            <div class='col2'>
+                <label>My Projects</label>
+                <input class='input-field' type='checkbox'>
+            </div>
+            <div class='col3'>
+                <button class='filter-btn btn'>Filter</button>
+            </div>
+        </div>
+        <div class='results'>
+            <div class='result' onmouseover="DisplayButtons('p-list-1')" onmouseout="HideButtons('p-list-1')">
+                <p class='request-id'>ProjectId</p>
+                <p class='request-id'>ProjectName</p>
+                <div class='buttons' id='p-list-1'>
+                    <button class='view-btn btn'>View</button>
+                </div>
+            </div>
+            <div class='result' onmouseover="DisplayButtons('p-list-2')" onmouseout="HideButtons('p-list-2')">
+                <p class='request-id'>ProjectId</p>
+                <p class='request-id'>ProjectName</p>
+                <div class='buttons' id='p-list-2'>
+                    <button class='view-btn btn'>View</button>
+                </div>
+            </div>
+            <div class='result' onmouseover="DisplayButtons('p-list-3')" onmouseout="HideButtons('p-list-3')">
+                <p class='request-id'>ProjectId</p>
+                <p class='request-id'>ProjectName</p>
+                <div class='buttons' id='p-list-3'>
+                    <button class='view-btn btn'>View</button>
+                </div>
+            </div>
+            <div class='result' onmouseover="DisplayButtons('p-list-4')" onmouseout="HideButtons('p-list-4')">
+                <p class='request-id'>ProjectId</p>
+                <p class='request-id'>ProjectName</p>
+                <div class='buttons' id='p-list-4'>
+                    <button class='view-btn btn'>View</button>
+                </div>
+            </div>
+            <div class='result' onmouseover="DisplayButtons('p-list-5')" onmouseout="HideButtons('p-list-5')">
+                <p class='request-id'>ProjectId</p>
+                <p class='request-id'>ProjectName</p>
+                <div class='buttons' id='p-list-5'>
+                    <button class='view-btn btn'>View</button>
+                </div>
+            </div>
+            <div class='result' onmouseover="DisplayButtons('p-list-6')" onmouseout="HideButtons('p-list-6')">
+                <p class='request-id'>ProjectId</p>
+                <p class='request-id'>ProjectName</p>
+                <div class='buttons' id='p-list-6'>
+                    <button class='view-btn btn'>View</button>
+                </div>
+            </div>
+            <div class='result' onmouseover="DisplayButtons('p-list-7')" onmouseout="HideButtons('p-list-7')">
+                <p class='request-id'>ProjectId</p>
+                <p class='request-id'>ProjectName</p>
+                <div class='buttons' id='p-list-7'>
+                    <button class='view-btn btn'>View</button>
+                </div>
+            </div>
+            <div class='result' onmouseover="DisplayButtons('p-list-8')" onmouseout="HideButtons('p-list-8')">
+                <p class='request-id'>ProjectId</p>
+                <p class='request-id'>ProjectName</p>
+                <div class='buttons' id='p-list-8'>
+                    <button class='view-btn btn'>View</button>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class='project-details'>
-
+    <div class='card project-details'>
+        <div class='title'>
+            Project Name
+        </div>
     </div>
-    <div class='project-committee'>
-
+    <div class='card project-committee'>
+        <div class='title'>
+            Committee Members
+        </div>
     </div>
 </div>
 
