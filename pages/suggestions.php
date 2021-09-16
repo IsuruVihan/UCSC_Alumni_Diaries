25ca24f66f9fc70d907a55b1dd9af70432d9e0d2
Shakya19020406 <shakya19020406@gmail.com>
2021-09-16 10:58:02 +0530

    suggestion form sketch done

--- pages/suggestions.php
+++ pages/suggestions.php
@@ -1,7 +1,30 @@
+<link rel='stylesheet' href='../assets/styles/suggestions.css'/>
+<link rel="stylesheet" href='https://pro.fontawesome.com/releases/v5.10.0/css/all.css'
+      integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p' crossorigin='anonymous'/>
+
+
 <?php include('../components/header.php'); ?>
 
+<div class='main-container'>
+    <p class='breadcrumb'>
+        <a href='home.php'>Home</a> / Suggestions
+    </p>
+    <p class='main-title'>
+	<i class="fas fa-lightbulb"></i> Suggestions
+    </p>
+</div>
+
 <div class='suggestions'>
-	Suggestions
+	<div class='card suggestions-box'>
+		<div class='title'>
+            Any Suggestions?        
+		</div>
+
+	
+
+    </div>
+
 </div>
 
 <?php include('../components/footer.php'); ?>
+
