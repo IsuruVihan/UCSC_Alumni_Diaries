35d67a2ebda8a272b9d2acc6aec9c3b251f3a20f
jayaweera2 <subhagyajayaweera1998@gmail.com>
2021-09-16 11:13:17 +0530

    group chat full skretch

---
+++ pages/group-chat.php
@@ -0,0 +1,17 @@
+<link rel='stylesheet' href='../assets/styles/group-chat.css'/>
+<link rel="stylesheet" href='https://pro.fontawesome.com/releases/v5.10.0/css/all.css'
+      integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p' crossorigin='anonymous'/>
+
+<?php include('../components/header.php'); ?>
+
+<div class='main-container'>
+    <p class='breadcrumb'>
+        <a href='home.php'>Home</a> /
+        <a href='admin.php'>Admin</a> / Accounts
+    </p>
+    <p class='main-title'>
+        <i class="fas fa-comments"></i> Group Chat
+    </p>
+</div>
+
+<?php include('../components/footer.php'); ?>
