a491536049c39afebd87c6f98839e4160ebe4e3b
IsuruVihan <isuruvihan@gmail.com>
2021-09-16 10:10:26 +0530

    Breadcrumb done

--- pages/projects.php
+++ pages/projects.php
@@ -1,7 +1,18 @@
 <?php include('../components/header.php'); ?>
+<link rel="stylesheet" href='https://pro.fontawesome.com/releases/v5.10.0/css/all.css'
+          integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p' crossorigin='anonymous'/>
+<link rel='stylesheet' href='../assets/styles/projects.css' />
 
+<div class='main-container'>
+    <p class='breadcrumb'>
+        <a href='home.php'>Home</a> / Projects
+    </p>
+    <p class='main-title'>
+        <i class="fas fa-project-diagram"></i> Projects
+    </p>
+</div>
 <div class='projects'>
-	Projects
+    Projects
 </div>
 
 <?php include('../components/footer.php'); ?>
