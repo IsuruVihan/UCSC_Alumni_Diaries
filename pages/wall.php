798cf45d51d523bac2f700aa8ffd681aeaedf7b3
pinsaradhanika97 <pinsaradhanika@gmail.com>
2021-09-16 11:05:55 +0530

    completed full sketch [remove border colors]

--- pages/wall.php
+++ pages/wall.php
@@ -1,7 +1,26 @@
 <?php include('../components/header.php'); ?>
+<link rel="stylesheet" href="../assets/styles/wall.css">
 
+<div class='main-container'>
+    <p class='breadcrumb'>
+        <a href='home.php'>Home</a> /
+        Wall
+    </p>
+    <p class='main-title'>
+        <i class="fas fa-users-cog"></i> Wall
+    </p>
+</div>
 <div class='wall'>
-	Wall
+	<div class="important-notice">
+		<p class="title">Important Notices</p>
+		
+		
+
+	</div>
+
+	<div class="common-wall">
+	<p class="title">Common Wall</p>
+	</div>
 </div>
 
 <?php include('../components/footer.php'); ?>
