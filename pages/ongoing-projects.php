2cb365bbaf38f8a463cdf8361570aac52ac9ae4c
IsuruVihan <isuruvihan@gmail.com>
2021-09-21 08:07:03 +0530

    Sketch to be continue

--- pages/ongoing-projects.php
+++ pages/ongoing-projects.php
@@ -14,7 +14,30 @@
     </p>
 </div>
 <div class='ongoing-projects'>
-    Ongoing projects
+    <div class='project-list'>
+     akjfb
+    </div>
+    <div class='project-details'>
+    [weojfwe
+    </div>
+    <div class='committee-members'>
+    [wef[pwef
+    </div>
+    <div class='cash'>
+    [owe[ew
+    </div>
+    <div class='items'>
+[weof[wekf
+    </div>
+    <div class='cash-spent'>
+]pwekfp]wef
+    </div>
+    <div class='items-spent'>
+[wekfpwef
+    </div>
+    <div class='committee-chat'>
+    wmefo[mwe
+    </div>
 </div>
 
 <?php include('../components/footer.php'); ?>
