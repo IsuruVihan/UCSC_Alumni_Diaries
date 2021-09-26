c4c102966003c584b6a519a4415fed6dbfe669ac
Shakya19020406 <shakya19020406@gmail.com>
2021-09-26 16:33:31 +0530

    Admin reports UI/UX done

--- pages/admin-reports.php
+++ pages/admin-reports.php
@@ -1,7 +1,386 @@
+<link rel='stylesheet' href='../assets/styles/admin-reports.css'/>
+<link rel="stylesheet" href='https://pro.fontawesome.com/releases/v5.10.0/css/all.css'
+      integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p' crossorigin='anonymous'/>
+
+
+
 <?php include('../components/header.php'); ?>
 
-<div class='admin-reports'>
-  Reports
+<div class='main-container'>
+    <p class='breadcrumb'>
+        <a href='home.php'>Home</a> / 
+        <a href='admin.php'>Admin</a> / Reports
+    </p>
+    <p class='main-title'>
+    <i class="fas fa-exclamation-triangle"></i> Report Actions
+    </p>
+</div>
+
+<div class='main-box'>
+    <div class='post-reports'>
+        <div class='card reports'>
+            <div class='title'>
+                Post Reports
+            </div>
+            <div class='flexbox-container'>
+                <div class='flexbox-item'>
+                    <div class='rdetails'>
+                        <div class='propic'>
+                        <img class ='img' src='../assets/images/user-default.png' width='100%' height='' class='user-pic' alt='user-pic' />
+                        </div>
+                        <div class='reporter'>
+                            <div class='reporter-name'>
+                                <div class='fname'>
+                                    <div class='first-name list-details'> First Name (Reporter)
+                                    </div>
+                                </div>
+                                <div class='lname'>
+                                    <div class='last-name list-details'> Last Name (Reporter)
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='report-post'>
+                                <div class='id'>
+                                    <div class='post-id list-details'> Reported Post ID
+                                    </div> 
+                                </div>
+                                <div class='owner'>
+                                    <div class='post-owner list-details'> Post Owner
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='cause'>
+                                <div class='report-cause'>
+                                    <div class='the-cause list-details'> Cause for Reporting
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class='rdelete'>
+                    <input type='reset' class='delete-btn' value='Delete'>
+                    </div>
+                </div>   
+                <div class='flexbox-item'>
+                    <div class='rdetails'>
+                        <div class='propic'>
+                        <img class ='img' src='../assets/images/user-default.png' width='100%' height='' class='user-pic' alt='user-pic' />
+                        </div>
+                        <div class='reporter'>
+                            <div class='reporter-name'>
+                                <div class='fname'>
+                                    <div class='first-name list-details'> First Name (Reporter)
+                                    </div>
+                                </div>
+                                <div class='lname'>
+                                    <div class='last-name list-details'> Last Name (Reporter)
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='report-post'>
+                                <div class='id'>
+                                    <div class='post-id list-details'> Reported Post ID
+                                    </div> 
+                                </div>
+                                <div class='owner'>
+                                    <div class='post-owner list-details'> Post Owner
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='cause'>
+                                <div class='report-cause'>
+                                    <div class='the-cause list-details'> Cause for Reporting
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class='rdelete'>
+                    <input type='reset' class='delete-btn' value='Delete'>
+                    </div>
+                </div>   
+                <div class='flexbox-item'>
+                    <div class='rdetails'>
+                        <div class='propic'>
+                        <img class ='img' src='../assets/images/user-default.png' width='100%' height='' class='user-pic' alt='user-pic' />
+                        </div>
+                        <div class='reporter'>
+                            <div class='reporter-name'>
+                                <div class='fname'>
+                                    <div class='first-name list-details'> First Name (Reporter)
+                                    </div>
+                                </div>
+                                <div class='lname'>
+                                    <div class='last-name list-details'> Last Name (Reporter)
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='report-post'>
+                                <div class='id'>
+                                    <div class='post-id list-details'> Reported Post ID
+                                    </div> 
+                                </div>
+                                <div class='owner'>
+                                    <div class='post-owner list-details'> Post Owner
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='cause'>
+                                <div class='report-cause'>
+                                    <div class='the-cause list-details'> Cause for Reporting
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class='rdelete'>
+                    <input type='reset' class='delete-btn' value='Delete'>
+                    </div>
+                </div>   
+                <div class='flexbox-item'>
+                    <div class='rdetails'>
+                        <div class='propic'>
+                        <img class ='img' src='../assets/images/user-default.png' width='100%' height='' class='user-pic' alt='user-pic' />
+                        </div>
+                        <div class='reporter'>
+                            <div class='reporter-name'>
+                                <div class='fname'>
+                                    <div class='first-name list-details'> First Name (Reporter)
+                                    </div>
+                                </div>
+                                <div class='lname'>
+                                    <div class='last-name list-details'> Last Name (Reporter)
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='report-post'>
+                                <div class='id'>
+                                    <div class='post-id list-details'> Reported Post ID
+                                    </div> 
+                                </div>
+                                <div class='owner'>
+                                    <div class='post-owner list-details'> Post Owner
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='cause'>
+                                <div class='report-cause'>
+                                    <div class='the-cause list-details'> Cause for Reporting
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class='rdelete'>
+                    <input type='reset' class='delete-btn' value='Delete'>
+                    </div>
+                </div>   
+
+            </div>
+        </div>
+    </div>
+
+    <div class='comment-reports'>
+        <div class='card reports'>
+            <div class='title'>
+                Comment Reports
+            </div>
+            <div class='flexbox-container'>
+                <div class='flexbox-item'>
+                    <div class='rdetails'>
+                        <div class='propic'>
+                            <img class ='img' src='../assets/images/user-default.png' width='100%' height='' class='user-pic' alt='user-pic' />
+                        </div>
+                        <div class='c-reporter'>
+                            <div class='c-reporter-name'>
+                                <div class='cfname'>
+                                    <div class='cfirst-name list-details'> First Name (Reporter)
+                                    </div>
+                                </div>
+                                <div class='clname'>
+                                    <div class='clast-name list-details'> Last Name (Reporter)
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='c-report-comment'>
+                                <div class='cid'>
+                                    <div class='cpost-id list-details'> Reported Post ID
+                                    </div>
+                                </div>
+                                <div class='cowner'>
+                                    <div class='cpost-owner list-details'> Post Owner
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='commentdetails'>
+                                <div class='rcid'>
+                                    <div class='cpost-id list-details'> Reported Comment ID
+                                    </div>
+                                </div>
+                                <div class='rcowner'>
+                                    <div class='cpost-id list-details'> Comment Owner
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='rcause'>
+                                <div class='report-cause'>
+                                    <div class='thee-cause list-details'> Cause for Reporting
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class='rdelete'>
+                        <input type='reset' class='delete-btn' value='Delete'>
+                    </div>
+                </div>
+                <div class='flexbox-item'>
+                    <div class='rdetails'>
+                        <div class='propic'>
+                            <img class ='img' src='../assets/images/user-default.png' width='100%' height='' class='user-pic' alt='user-pic' />
+                        </div>
+                        <div class='c-reporter'>
+                            <div class='c-reporter-name'>
+                                <div class='cfname'>
+                                    <div class='cfirst-name list-details'> First Name (Reporter)
+                                    </div>
+                                </div>
+                                <div class='clname'>
+                                    <div class='clast-name list-details'> Last Name (Reporter)
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='c-report-comment'>
+                                <div class='cid'>
+                                    <div class='cpost-id list-details'> Reported Post ID
+                                    </div>
+                                </div>
+                                <div class='cowner'>
+                                    <div class='cpost-owner list-details'> Post Owner
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='commentdetails'>
+                                <div class='rcid'>
+                                    <div class='cpost-id list-details'> Reported Comment ID
+                                    </div>
+                                </div>
+                                <div class='rcowner'>
+                                    <div class='cpost-id list-details'> Comment Owner
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='rcause'>
+                                <div class='report-cause'>
+                                    <div class='thee-cause list-details'> Cause for Reporting
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class='rdelete'>
+                        <input type='reset' class='delete-btn' value='Delete'>
+                    </div>
+                </div>
+                <div class='flexbox-item'>
+                    <div class='rdetails'>
+                        <div class='propic'>
+                            <img class ='img' src='../assets/images/user-default.png' width='100%' height='' class='user-pic' alt='user-pic' />
+                        </div>
+                        <div class='c-reporter'>
+                            <div class='c-reporter-name'>
+                                <div class='cfname'>
+                                    <div class='cfirst-name list-details'> First Name (Reporter)
+                                    </div>
+                                </div>
+                                <div class='clname'>
+                                    <div class='clast-name list-details'> Last Name (Reporter)
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='c-report-comment'>
+                                <div class='cid'>
+                                    <div class='cpost-id list-details'> Reported Post ID
+                                    </div>
+                                </div>
+                                <div class='cowner'>
+                                    <div class='cpost-owner list-details'> Post Owner
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='commentdetails'>
+                                <div class='rcid'>
+                                    <div class='cpost-id list-details'> Reported Comment ID
+                                    </div>
+                                </div>
+                                <div class='rcowner'>
+                                    <div class='cpost-id list-details'> Comment Owner
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='rcause'>
+                                <div class='report-cause'>
+                                    <div class='thee-cause list-details'> Cause for Reporting
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class='rdelete'>
+                        <input type='reset' class='delete-btn' value='Delete'>
+                    </div>
+                </div>
+                <div class='flexbox-item'>
+                    <div class='rdetails'>
+                        <div class='propic'>
+                            <img class ='img' src='../assets/images/user-default.png' width='100%' height='' class='user-pic' alt='user-pic' />
+                        </div>
+                        <div class='c-reporter'>
+                            <div class='c-reporter-name'>
+                                <div class='cfname'>
+                                    <div class='cfirst-name list-details'> First Name (Reporter)
+                                    </div>
+                                </div>
+                                <div class='clname'>
+                                    <div class='clast-name list-details'> Last Name (Reporter)
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='c-report-comment'>
+                                <div class='cid'>
+                                    <div class='cpost-id list-details'> Reported Post ID
+                                    </div>
+                                </div>
+                                <div class='cowner'>
+                                    <div class='cpost-owner list-details'> Post Owner
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='commentdetails'>
+                                <div class='rcid'>
+                                    <div class='cpost-id list-details'> Reported Comment ID
+                                    </div>
+                                </div>
+                                <div class='rcowner'>
+                                    <div class='cpost-id list-details'> Comment Owner
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='rcause'>
+                                <div class='report-cause'>
+                                    <div class='thee-cause list-details'> Cause for Reporting
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class='rdelete'>
+                        <input type='reset' class='delete-btn' value='Delete'>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php include('../components/footer.php'); ?>
