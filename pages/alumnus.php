47dfef185a2efb97740bce949dabf189d60565c8
pinsaradhanika97 <pinsaradhanika@gmail.com>
2021-09-30 14:23:50 +0530

    complted ui-ux of registered alumni

--- pages/alumnus.php
+++ pages/alumnus.php
@@ -15,19 +15,19 @@
 </div>
 
 <div class='alumnus-container'>
-	<header class='grid-title'>Registered Alumnus</header>
+	
 	<div class='grid-container'>
 		<div class='account-details-container'>
 			<div class='img-container'>
 
 			</div>
 			<div class='acdc-text-field'>Account Type</div>
-			<div class='acdc-text-field'>Remove Account </div>
-			<div class='acdc-text-field'>Ban Account</div>
+			<button class='btn acdc-btn'>Remove Account </button>
+			<button class='btn acdc-btn'>Ban Account</button>
 			<div class='acdc-div-container'>
 					<div class='acdc-text-field'>Subscription Type</div>
 					<div class='acdc-text-field'>Due Date </div>
-					<div class='acdc-text-field'>Get Recharge Details</div>	
+					<button class='btn acdc-btn'>Recharge Details</button>	
 			</div>
 		</div>
 		<div class='alumni-details-container'>
@@ -47,13 +47,210 @@
 		</div>
 
 		<div class='r-alumni-list-container'>
-
+			<header class='container-heading'>Registered Alumni List </header>
+			<div class='filter-container'>
+				<div class='filter-container-col1'>
+					<input type='text' placeholder='First Name' class='input-field-title'>
+					<input type='text' placeholder='Last Name' class='input-field-title'>
+					<select class='input-field-title'>
+						<option value='All'>All</option>
+						<option value='2018/2019'>2018/2019</option>
+						<option value='2018/2019'>2019/2020</option>
+						<option value='2018/2019'>2020/2021</option>
+					</select>
+				</div>
+				<div class='filter-container-col2'>
+					<button class='btn filter-btn'>Filter</button>
+				</div>
+			</div>
+			<div class='alumni-list-box'>
+				<div class='alumni-box-row'>
+					<div class='list-prof-pic'> </div>
+					<div class='list-detail-field'>First name</div>
+					<div class='list-detail-field'>Last name</div>
+					<div class='list-detail-field'>Batch</div>
+					<button class='btn view-btn'>View</button>
+				</div>
+				<div class='alumni-box-row'>
+					<div class='list-prof-pic'> </div>
+					<div class='list-detail-field'>First name</div>
+					<div class='list-detail-field'>Last name</div>
+					<div class='list-detail-field'>Batch</div>
+					<button class='btn view-btn'>View</button>
+				</div>
+				<div class='alumni-box-row'>
+					<div class='list-prof-pic'> </div>
+					<div class='list-detail-field'>First name</div>
+					<div class='list-detail-field'>Last name</div>
+					<div class='list-detail-field'>Batch</div>
+					<button class='btn view-btn'>View</button>
+				</div>
+				<div class='alumni-box-row'>
+					<div class='list-prof-pic'> </div>
+					<div class='list-detail-field'>First name</div>
+					<div class='list-detail-field'>Last name</div>
+					<div class='list-detail-field'>Batch</div>
+					<button class='btn view-btn'>View</button>
+				</div>
+				<div class='alumni-box-row'>
+					<div class='list-prof-pic'> </div>
+					<div class='list-detail-field'>First name</div>
+					<div class='list-detail-field'>Last name</div>
+					<div class='list-detail-field'>Batch</div>
+					<button class='btn view-btn'>View</button>
+				</div>
+				<div class='alumni-box-row'>
+					<div class='list-prof-pic'> </div>
+					<div class='list-detail-field'>First name</div>
+					<div class='list-detail-field'>Last name</div>
+					<div class='list-detail-field'>Batch</div>
+					<button class='btn view-btn'>View</button>
+				</div>
+				<div class='alumni-box-row'>
+					<div class='list-prof-pic'> </div>
+					<div class='list-detail-field'>First name</div>
+					<div class='list-detail-field'>Last name</div>
+					<div class='list-detail-field'>Batch</div>
+					<button class='btn view-btn'>View</button>
+				</div>
+				<div class='alumni-box-row'>
+					<div class='list-prof-pic'> </div>
+					<div class='list-detail-field'>First name</div>
+					<div class='list-detail-field'>Last name</div>
+					<div class='list-detail-field'>Batch</div>
+					<button class='btn view-btn'>View</button>
+				</div>
+				<div class='alumni-box-row'>
+					<div class='list-prof-pic'> </div>
+					<div class='list-detail-field'>First name</div>
+					<div class='list-detail-field'>Last name</div>
+					<div class='list-detail-field'>Batch</div>
+					<button class='btn view-btn'>View</button>
+				</div>
+				
+				
+			</div>
+			
 		</div>
+
 		<div class='contributions-container'>
-
+			<header class='container-heading'>Contributions </header>
+			<div class='contribution-box'>	
+				<div class='contribution-row1'>
+						<p class='request-id'>FirstName</p>
+						<p class='request-id'>LastName</p>
+						<p class='request-id'>Timestamp</p>
+				</div>
+				<div class='contribution-row1'>
+						<p class='request-id'>FirstName</p>
+						<p class='request-id'>LastName</p>
+						<p class='request-id'>Timestamp</p>
+				</div>
+				<div class='contribution-row1'>
+						<p class='request-id'>FirstName</p>
+						<p class='request-id'>LastName</p>
+						<p class='request-id'>Timestamp</p>
+				</div>
+				<div class='contribution-row1'>
+						<p class='request-id'>FirstName</p>
+						<p class='request-id'>LastName</p>
+						<p class='request-id'>Timestamp</p>
+				</div>
+				<div class='contribution-row1'>
+						<p class='request-id'>FirstName</p>
+						<p class='request-id'>LastName</p>
+						<p class='request-id'>Timestamp</p>
+				</div>
+				<div class='contribution-row1'>
+						<p class='request-id'>FirstName</p>
+						<p class='request-id'>LastName</p>
+						<p class='request-id'>Timestamp</p>
+				</div>
+				<div class='contribution-row1'>
+						<p class='request-id'>FirstName</p>
+						<p class='request-id'>LastName</p>
+						<p class='request-id'>Timestamp</p>
+				</div>
+				<div class='contribution-row1'>
+						<p class='request-id'>FirstName</p>
+						<p class='request-id'>LastName</p>
+						<p class='request-id'>Timestamp</p>
+				</div>
+				<div class='contribution-row1'>
+						<p class='request-id'>FirstName</p>
+						<p class='request-id'>LastName</p>
+						<p class='request-id'>Timestamp</p>
+				</div>
+				<div class='contribution-row1'>
+						<p class='request-id'>FirstName</p>
+						<p class='request-id'>LastName</p>
+						<p class='request-id'>Timestamp</p>
+				</div>
+				<div class='contribution-row1'>
+						<p class='request-id'>FirstName</p>
+						<p class='request-id'>LastName</p>
+						<p class='request-id'>Timestamp</p>
+				</div>
+				<div class='contribution-row1'>
+						<p class='request-id'>FirstName</p>
+						<p class='request-id'>LastName</p>
+						<p class='request-id'>Timestamp</p>
+				</div>
+				
+				
+			</div>	
 		</div>
-		<div class='involved-projects-container'>
 
+		<div class='involved-projects-container'>
+			<header class='container-heading'>Involved Projects </header>
+			<div class='involved-projects-box'>
+				<div class='involved-projects-row'>
+					<p>Project Name</p>
+					<p>Position</p>
+				</div>
+				<div class='involved-projects-row'>
+					<p>Project Name</p>
+					<p>Position</p>
+				</div>
+				<div class='involved-projects-row'>
+					<p>Project Name</p>
+					<p>Position</p>
+				</div>
+				<div class='involved-projects-row'>
+					<p>Project Name</p>
+					<p>Position</p>
+				</div>
+				<div class='involved-projects-row'>
+					<p>Project Name</p>
+					<p>Position</p>
+				</div>
+				<div class='involved-projects-row'>
+					<p>Project Name</p>
+					<p>Position</p>
+				</div>
+				<div class='involved-projects-row'>
+					<p>Project Name</p>
+					<p>Position</p>
+				</div>
+				<div class='involved-projects-row'>
+					<p>Project Name</p>
+					<p>Position</p>
+				</div>
+				<div class='involved-projects-row'>
+					<p>Project Name</p>
+					<p>Position</p>
+				</div>
+				<div class='involved-projects-row'>
+					<p>Project Name</p>
+					<p>Position</p>
+				</div>
+				<div class='involved-projects-row'>
+					<p>Project Name</p>
+					<p>Position</p>
+				</div>
+				
+				
+			</div>
 		</div>
 	</div>
 </div>
