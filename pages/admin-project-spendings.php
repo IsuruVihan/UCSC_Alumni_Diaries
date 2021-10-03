b57c66de29a0c305e18c7a27ad8077906ee163bf
pinsaradhanika97 <pinsaradhanika@gmail.com>
2021-10-03 23:08:02 +0530

    project spendings ui-ux done

--- pages/admin-project-spendings.php
+++ pages/admin-project-spendings.php
@@ -1,7 +1,190 @@
 <?php include('../components/header.php'); ?>
+<link rel="stylesheet" href="../assets/styles/project-spendings.css">
+<script src="https://use.fontawesome.com/3e9045be79.js"></script>
 
+<div class='main-container'>
+    <p class='breadcrumb'>
+        <a href='home.php'>Home</a> /
+       <a href="admin.php">Admin</a> / Project Spending
+    </p>
+    <p class='main-title'>
+    <i class="fa fa-address-card" ></i> Project Spending
+    </p>
+</div>
 <div class='admin-project-spendings'>
-  Project Spendings
+    
+        <div class='container-col-1'>
+          <div class='container-heading'>All Projects</div>
+              <div class='filter-box'>
+                  <input class='input-field date-field' type='date' />To
+                  <input class='input-field date-field' type='date' />
+                  <input class='input-field text-field' type='text' placeholder='Coordinator '/> 
+                  <select class='input-field drop-down'>
+                      <option value="All Projects">All Projects</option>
+                      <option value="All Projects">Not Yet Started Projects</option>
+                      <option value="All Projects">Ongoing Projects</option>
+                      <option value="All Projects">Completed Projects</option>
+                      <option value="All Projects">Closed Projects</option>
+                  </select>
+                  <button class='filter-btn btn'>Filter</button>
+              </div>
+            <div class='scroll-box1'>  
+              <div class='project-description'>
+                  <div class='project-detail-box'>
+                    <div class='project-detail-box-col1'>
+                      <div class='p-name details-field'>Project Name</div>
+                      <div class='p-s-date details-field'>Start Date</div>
+                      <div class='p-e-date details-field'>End Date</div>
+                      <div class='p-cordi details-field'>Coordinator</div>
+                    </div>  
+                    <div class='project-detail-box-col2'>
+                      <div class='p-description details-field '>Description</div>
+                      <button class='p-view view-btn btn'>View</button>
+                    </div>  
+                  </div>
+              </div>
+              <div class='project-description'>
+                  <div class='project-detail-box'>
+                    <div class='project-detail-box-col1'>
+                      <div class='p-name details-field'>Project Name</div>
+                      <div class='p-s-date details-field'>Start Date</div>
+                      <div class='p-e-date details-field'>End Date</div>
+                      <div class='p-cordi details-field'>Coordinator</div>
+                    </div>  
+                    <div class='project-detail-box-col2'>
+                      <div class='p-description details-field '>Description</div>
+                      <button class='p-view view-btn btn'>View</button>
+                    </div>  
+                  </div>
+              </div>
+              <div class='project-description'>
+                  <div class='project-detail-box'>
+                    <div class='project-detail-box-col1'>
+                      <div class='p-name details-field'>Project Name</div>
+                      <div class='p-s-date details-field'>Start Date</div>
+                      <div class='p-e-date details-field'>End Date</div>
+                      <div class='p-cordi details-field'>Coordinator</div>
+                    </div>  
+                    <div class='project-detail-box-col2'>
+                      <div class='p-description details-field '>Description</div>
+                      <button class='p-view view-btn btn'>View</button>
+                    </div>  
+                  </div>
+              </div>
+              <div class='project-description'>
+                  <div class='project-detail-box'>
+                    <div class='project-detail-box-col1'>
+                      <div class='p-name details-field'>Project Name</div>
+                      <div class='p-s-date details-field'>Start Date</div>
+                      <div class='p-e-date details-field'>End Date</div>
+                      <div class='p-cordi details-field'>Coordinator</div>
+                    </div>  
+                    <div class='project-detail-box-col2'>
+                      <div class='p-description details-field '>Description</div>
+                      <button class='p-view view-btn btn'>View</button>
+                    </div>  
+                  </div>
+              </div>
+              <div class='project-description'>
+                  <div class='project-detail-box'>
+                    <div class='project-detail-box-col1'>
+                      <div class='p-name details-field'>Project Name</div>
+                      <div class='p-s-date details-field'>Start Date</div>
+                      <div class='p-e-date details-field'>End Date</div>
+                      <div class='p-cordi details-field'>Coordinator</div>
+                    </div>  
+                    <div class='project-detail-box-col2'>
+                      <div class='p-description details-field '>Description</div>
+                      <button class='p-view view-btn btn'>View</button>
+                    </div>  
+                  </div>
+              </div>
+              <div class='project-description'>
+                  <div class='project-detail-box'>
+                    <div class='project-detail-box-col1'>
+                      <div class='p-name details-field'>Project Name</div>
+                      <div class='p-s-date details-field'>Start Date</div>
+                      <div class='p-e-date details-field'>End Date</div>
+                      <div class='p-cordi details-field'>Coordinator</div>
+                    </div>  
+                    <div class='project-detail-box-col2'>
+                      <div class='p-description details-field '>Description</div>
+                      <button class='p-view view-btn btn'>View</button>
+                    </div>  
+                  </div>
+              </div>
+            </div>
+        </div>
+        <div class='container-col-2'>
+            <div class='cash-spendings'>  
+              <div class='container-heading'>Cash Spendings</div>
+              <div class='cash-spendings-row1'>
+                  <div class='spend-detail'>
+                    <div class='cash-spend-description details-field '>Description</div>
+                    <div class='details-field timestamp'>Timestamp</div>
+                    <div class='details-field bill-attach'>Bill </div>
+                    <div class='details-field cost'>Cost</div>
+                 </div>
+                 <div class='spend-detail'>
+                    <div class='cash-spend-description details-field '>Description</div>
+                    <div class='details-field timestamp'>Timestamp</div>
+                    <div class='details-field bill-attach'>Bill </div>
+                    <div class='details-field cost'>Cost</div>
+                 </div>
+                  <div class='spend-detail'>
+                    <div class='cash-spend-description details-field '>Description</div>
+                    <div class='details-field timestamp'>Timestamp</div>
+                    <div class='details-field bill-attach'>Bill </div>
+                    <div class='details-field cost'>Cost</div>
+                  </div>
+                  <div class='spend-detail'>
+                    <div class='cash-spend-description details-field '>Description</div>
+                    <div class='details-field timestamp'>Timestamp</div>
+                    <div class='details-field bill-attach'>Bill </div>
+                    <div class='details-field cost'>Cost</div>
+                  </div>
+                  <div class='spend-detail'>
+                    <div class='cash-spend-description details-field '>Description</div>
+                    <div class='details-field timestamp'>Timestamp</div>
+                    <div class='details-field bill-attach'>Bill </div>
+                    <div class='details-field cost'>Cost</div>
+                  </div>
+               </div>  
+               <div class='cash-spendings-row2'>
+                 <button class='generate-report-btn btn'>Generate Report</button>
+                 <div class='details-field total'>Sub Total</div>
+               </div> 
+          </div>
+           <div class='item-spendings'>
+                  <div class='container-heading'>Item Spendings</div>
+                <div class='cash-spendings-row1'>
+                      <div class='spend-detail'>
+                          <div class='details-field item-name'>Item Name</div>
+                          <div class='cash-spend-description details-field '>Description</div>
+                          <div class='details-field timestamp'>Timestamp</div>
+                          <div class='details-field bill-attach'>Bill </div>
+                          <div class='details-field qty'>Qty</div>
+                        </div>
+                        <div class='spend-detail'>
+                          <div class='details-field item-name'>Item Name</div>
+                          <div class='cash-spend-description details-field '>Description</div>
+                          <div class='details-field timestamp'>Timestamp</div>
+                          <div class='details-field bill-attach'>Bill </div>
+                          <div class='details-field qty'>Qty</div>
+                        </div>
+                        <div class='spend-detail'>
+                          <div class='details-field item-name'>Item Name</div>
+                          <div class='cash-spend-description details-field '>Description</div>
+                          <div class='details-field timestamp'>Timestamp</div>
+                          <div class='details-field bill-attach'>Bill </div>
+                          <div class='details-field qty'>Qty</div>
+                        </div>
+                 </div>
+                  <div class='cash-spendings-row2 item-spend-row'>
+                      <button class='generate-report-btn btn'>Generate Report</button>
+                  </div>
+             </div>
+        </div>
 </div>
 
 <?php include('../components/footer.php'); ?>
