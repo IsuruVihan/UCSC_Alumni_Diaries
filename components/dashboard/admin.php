388805dcb9bf1e2630dcc647db280ff495bd9eb8
IsuruVihan <isuruvihan@gmail.com>
2021-10-27 17:12:39 +0530

    Admin dashboard UI done

---
+++ components/dashboard/admin.php
@@ -0,0 +1,80 @@
+<link rel='stylesheet' href='../../assets/styles/admin-dashboard.css'/>
+
+<div class='admin-dashboard'>
+    <div class='section-1 card-section'>
+        <div class='card card-width-1'>
+            <div class='title'>Registered Members</div>
+            <div class='count'>100</div>
+        </div>
+        <div class='card card-width-1'>
+            <div class='title'>Account Requests</div>
+            <div class='count'>100</div>
+        </div>
+        <div class='card card-width-1'>
+            <div class='title'>Rejected Requests</div>
+            <div class='count'>100</div>
+        </div>
+        <div class='card card-width-1'>
+            <div class='title'>Banned Accounts</div>
+            <div class='count'>100</div>
+        </div>
+    </div>
+    <div class='section-2 card-section'>
+        <div class='card card-width-1'>
+            <div class='title'>Not started projects</div>
+            <div class='count'>100</div>
+        </div>
+        <div class='card card-width-1'>
+            <div class='title'>Ongoing<br/>Projects</div>
+            <div class='count'>100</div>
+        </div>
+        <div class='card card-width-1'>
+            <div class='title'>Completed Projects</div>
+            <div class='count'>100</div>
+        </div>
+        <div class='card card-width-1'>
+            <div class='title'>Closed<br/>Projects</div>
+            <div class='count'>100</div>
+        </div>
+    </div>
+    <div class='section-3 card-section'>
+        <div class='card card-width-2'>
+            <div class='title'>Available Cash<br/<b>(LKR)</b></div>
+            <div class='count'>100</div>
+        </div>
+        <div class='card card-width-2'>
+            <div class='title'>Cash Donation<br/<b>(LKR)</b></div>
+            <div class='count'>100</div>
+        </div>
+        <div class='card card-width-2'>
+            <div class='title'>Subscriptions<br/<b>(LKR)</b></div>
+            <div class='count'>100</div>
+        </div>
+        <div class='card card-width-2'>
+            <div class='title'>Subscriptions to be accepted</div>
+            <div class='count'>100</div>
+        </div>
+        <div class='card card-width-2'>
+            <div class='title'>Cash donations to be accepted</div>
+            <div class='count'>100</div>
+        </div>
+        <div class='card card-width-2'>
+            <div class='title'>Item donations to be accepted</div>
+            <div class='count'>100</div>
+        </div>
+    </div>
+    <div class='section-4 card-section'>
+        <div class='card card-width-3'>
+            <div class='title'>Suggestions</div>
+            <div class='count'>100</div>
+        </div>
+        <div class='card card-width-3'>
+            <div class='title'>Post Reports</div>
+            <div class='count'>100</div>
+        </div>
+        <div class='card card-width-3'>
+            <div class='title'>Comment Reports</div>
+            <div class='count'>100</div>
+        </div>
+    </div>
+</div>
--- pages/home.php
+++ pages/home.php
@@ -4,45 +4,69 @@
 
 <?php include('../components/header.php'); ?>
 
+<div class='main-container'>
+    <p class='main-title'>
+        <i class="fas fa-chart-line"></i> Dashboard
+    </p>
+</div>
 <div class='home'>
-    <div class='white-space'></div>
-    <img src='../assets/images/main-cover%20-%20Copy.jpg' class='main-cover' alt="main-cover"/>
-    <div class='title'>
-        Who we are?
+<!--    <div class='white-space'></div>-->
+    <div class='dashboard-links'>
+        <a
+            class='dashboard-link clicked-link'
+            href='../components/dashboard/general.php'
+            target='dashboard'
+            id='general-tab'
+            onclick=onClickGeneral()
+        >General</a>
+        <a
+            class='dashboard-link'
+            href='../components/dashboard/admin.php'
+            target='dashboard'
+            id='admin-tab'
+            onclick=onClickAdmin()
+        >Admin</a>
     </div>
-    <div class='text'>
-        <div class='para'>
-            <img src='../assets/gifs/cover-1.gif' width='33%' alt="main-cover"/>
-            <p class='para-text'>
-                Welcome to the official web site of the Alumni Association of University of Colombo of School of
-                Computing
-                (UCSC). Alumni Association of University of Colombo School of Computing is formed in the sole objective
-                of
-                enriching the fellowship among Graduate and Undergraduate Community to initiate and hold responsibility
-                to en
-                route the development of UCSC in achieving highest educational, professional and ethical standards by
-                acting as
-                an independent entity.
-            </p>
-        </div>
-        <div class='para'>
-            <img src='../assets/gifs/cover-2.gif' width='33%' alt="main-cover"/>
-            <p class='para-text'>
-                The accomplishment of the association would also focus on the process enhancing the education standard
-                and usage
-                of ICT throughout the country.
-            </p>
-        </div>
-        <div class='para'>
-            <img src='../assets/gifs/cover-3.gif' width='33%' alt="main-cover"/>
-            <p class='para-text'>
-                As a strong entity with having vastly talented professionals in the modern era,
-                we the association, will make it our passion of achieving the above goals by accomplishing its
-                predefined
-                targets throughout.
-            </p>
-        </div>
-    </div>
+    <div class='white-space2'></div>
+    <iframe name='dashboard' class='dashboard' src='../components/dashboard/general.php'></iframe>
+    
+<!--    <img src='../assets/images/main-cover%20-%20Copy.jpg' class='main-cover' alt="main-cover"/>-->
+<!--    <div class='title'>-->
+<!--        Who we are?-->
+<!--    </div>-->
+<!--    <div class='text'>-->
+<!--        <div class='para'>-->
+<!--            <img src='../assets/gifs/cover-1.gif' width='33%' alt="main-cover"/>-->
+<!--            <p class='para-text'>-->
+<!--                Welcome to the official web site of the Alumni Association of University of Colombo of School of-->
+<!--                Computing-->
+<!--                (UCSC). Alumni Association of University of Colombo School of Computing is formed in the sole objective-->
+<!--                of-->
+<!--                enriching the fellowship among Graduate and Undergraduate Community to initiate and hold responsibility-->
+<!--                to en-->
+<!--                route the development of UCSC in achieving highest educational, professional and ethical standards by-->
+<!--                acting as-->
+<!--                an independent entity.-->
+<!--            </p>-->
+<!--        </div>-->
+<!--        <div class='para'>-->
+<!--            <img src='../assets/gifs/cover-2.gif' width='33%' alt="main-cover"/>-->
+<!--            <p class='para-text'>-->
+<!--                The accomplishment of the association would also focus on the process enhancing the education standard-->
+<!--                and usage-->
+<!--                of ICT throughout the country.-->
+<!--            </p>-->
+<!--        </div>-->
+<!--        <div class='para'>-->
+<!--            <img src='../assets/gifs/cover-3.gif' width='33%' alt="main-cover"/>-->
+<!--            <p class='para-text'>-->
+<!--                As a strong entity with having vastly talented professionals in the modern era,-->
+<!--                we the association, will make it our passion of achieving the above goals by accomplishing its-->
+<!--                predefined-->
+<!--                targets throughout.-->
+<!--            </p>-->
+<!--        </div>-->
+<!--    </div>-->
 </div>
 
 <script src='../js/home.js'></script>
