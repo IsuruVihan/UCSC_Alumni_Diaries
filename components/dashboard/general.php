3df19b3817e41983cc5f9d0e41f08755385c38ba
IsuruVihan <isuruvihan@gmail.com>
2021-10-28 12:26:12 +0530

    General dashboard stat cards done

--- components/dashboard/general.php
+++ components/dashboard/general.php
@@ -1,1 +1,34 @@
-<p>General Dashboard</p>
+<link rel='stylesheet' href='../../assets/styles/general-dashboard.css'/>
+
+<div class='general-dashboard'>
+    <div class='section-1'>
+        <div class='card c-1'>
+            <div class='title'>Registered Members</div>
+            <div class='count'>100</div>
+        </div>
+        <div class='card c-2'>
+            <div class='title'>Completed Projects</div>
+            <div class='count'>100</div>
+        </div>
+        <div class='card c-3'>
+            <div class='title'>Notifications</div>
+            <div class='count'>100</div>
+        </div>
+        <div class='card c-4'>
+            <div class='title'>My Projects</div>
+            <div class='count'>100</div>
+        </div>
+    </div>
+    <div class='section-2'>
+        <div class='top'>
+            Top Donors
+        </div>
+        <div class='header'>
+            <span class='title'>Name</span>
+            <span class='title'>Amount (LKR)</span>
+        </div>
+        <div class='result'>
+        
+        </div>
+    </div>
+</div>
