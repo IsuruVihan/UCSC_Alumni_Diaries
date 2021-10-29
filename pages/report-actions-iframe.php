80959b95a5ac9a07e96ecf1bf185ef3a9760ad23
IsuruVihan <isuruvihan@gmail.com>
2021-10-29 12:58:21 +0530

    Removed previous version

--- pages/report-actions-iframe.php
+++ pages/report-actions-iframe.php
@@ -1,4 +1,4 @@
-<link rel='stylesheet' href='../assets/styles/report-actions-iframe.css'/>
+<link rel='stylesheet' href='../assets/styles/admin-reports.css'/>
 <link rel="stylesheet" href='https://pro.fontawesome.com/releases/v5.10.0/css/all.css'
       integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p' crossorigin='anonymous'/>
 
