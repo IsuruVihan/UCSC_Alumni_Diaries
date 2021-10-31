cf24a1eeebc0f45f338a113b1ce1cb0775f62751
Sahani19020821 <sahani19020821@gmail.com>
2021-10-31 11:45:02 +0530

    Private Chat Iframe done

--- js/private-chat.js
+++ js/private-chat.js
@@ -1,2 +1,28 @@
-const message = document.getElementById('message-list');
-message.scrollTop= message.scrollHeight;
+const chatListLink = document.getElementById('link-1');
+const availUsers = document.getElementById('link-2');
+
+const onClickChatList = () => {
+    chatListLink.classList.add('clicked-link');
+    availUsers.classList.remove('clicked-link');
+}
+
+const onClickAvailUsers = () => {
+    chatListLink.classList.remove('clicked-link');
+    availUsers.classList.add('clicked-link');
+}
+
+const chatList = document.getElementById('chat-list');
+const chatWindow = document.getElementById('chat');
+
+
+const onClickViewBtn = () => {
+    chatList.style.display= "none";
+    chatWindow.style.display = "flex";
+    const message =document.getElementById('message-list');
+    message.scrollTop = message.scrollHeight;
+}
+
+const onClickChatListBtn = () => {
+    chatList.style.display= "flex";
+    chatWindow.style.display = "none";
+}
