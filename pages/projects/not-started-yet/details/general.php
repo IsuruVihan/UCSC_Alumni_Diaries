91ee64aab6269b918c70dc7ac2dca06d52d2e3cc
IsuruVihan <isuruvihan@gmail.com>
2021-10-31 17:12:31 +0530

    not-started-yet project-details-general done

--- pages/projects/not-started-yet/details/general.php
+++ pages/projects/not-started-yet/details/general.php
@@ -1,1 +1,19 @@
-General
+<link rel='stylesheet' href='../../../../assets/styles/not-started-yet-details-general.css'/>
+<link rel='stylesheet' href='https://pro.fontawesome.com/releases/v5.10.0/css/all.css'
+      integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p' crossorigin='anonymous'/>
+
+<div class='general'>
+    <form class='general-details-form'>
+        <label for='p-name' class='label'>Project name</label>
+        <input id='p-name' class='input' type='text'/>
+        <label for='p-description' class='label'>Project description</label>
+        <textarea class='input text-area' id='p-description'></textarea>
+        <div class='notice'>
+            * You have unsaved changes.
+        </div>
+        <div class='buttons'>
+            <input class='button save-btn' type='submit' value='Save changes' />
+            <button class='button cancel-btn'>Cancel</button>
+        </div>
+    </form>
+</div>
