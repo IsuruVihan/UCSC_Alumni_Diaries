62613bb822a7ebe4e210e0f3ebcaff2c7f6f2794
Sahani19020821 <sahani19020821@gmail.com>
2021-11-01 16:09:32 +0530

    Cleaned contribution php file with proper indentation

--- pages/my-account/contribution.php
+++ pages/my-account/contribution.php
@@ -2,40 +2,40 @@
 <link rel="stylesheet" href='https://pro.fontawesome.com/releases/v5.10.0/css/all.css'
       integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p' crossorigin='anonymous'/>
 
- <div class='contributions'>
-            <div class='title'>
-                Contributions
-            </div>
-            <div class='contributions-container'>
-                <div class='contributions-item'>
-                    <p class='item-label'>ProjectName</p>
-                    <p class='item-label'>Amount</p>
-                    <p class='item-label'>time</p>
-                </div>
-                <div class='contributions-item'>
-                    <p class='item-label'>ProjectName</p>
-                    <p class='item-label'>Amount</p>
-                    <p class='item-label'>time</p>
-                </div>
-                <div class='contributions-item'>
-                    <p class='item-label'>ProjectName</p>
-                    <p class='item-label'>Amount</p>
-                    <p class='item-label'>time</p>
-                </div>
-                <div class='contributions-item'>
-                    <p class='item-label'>ProjectName</p>
-                    <p class='item-label'>Amount</p>
-                    <p class='item-label'>time</p>
-                </div>
-                <div class='contributions-item'>
-                    <p class='item-label'>ProjectName</p>
-                    <p class='item-label'>Amount</p>
-                    <p class='item-label'>time</p>
-                </div>
-                <div class='contributions-item'>
-                    <p class='item-label'>ProjectName</p>
-                    <p class='item-label'>Amount</p>
-                    <p class='item-label'>time</p>
-                </div>
-            </div>
+<div class='contributions'>
+    <div class='title'>
+        Contributions
+    </div>
+    <div class='contributions-container'>
+        <div class='contributions-item'>
+            <p class='item-label'>ProjectName</p>
+            <p class='item-label'>Amount</p>
+            <p class='item-label'>time</p>
+        </div>
+        <div class='contributions-item'>
+            <p class='item-label'>ProjectName</p>
+            <p class='item-label'>Amount</p>
+            <p class='item-label'>time</p>
+        </div>
+        <div class='contributions-item'>
+            <p class='item-label'>ProjectName</p>
+            <p class='item-label'>Amount</p>
+            <p class='item-label'>time</p>
+        </div>
+        <div class='contributions-item'>
+            <p class='item-label'>ProjectName</p>
+            <p class='item-label'>Amount</p>
+            <p class='item-label'>time</p>
+        </div>
+        <div class='contributions-item'>
+            <p class='item-label'>ProjectName</p>
+            <p class='item-label'>Amount</p>
+            <p class='item-label'>time</p>
+        </div>
+        <div class='contributions-item'>
+            <p class='item-label'>ProjectName</p>
+            <p class='item-label'>Amount</p>
+            <p class='item-label'>time</p>
+        </div>
+    </div>
 </div>
