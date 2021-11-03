bc61338ef1e07b8562f4f72f6e13b7c59836d1ac
IsuruVihan <isuruvihan@gmail.com>
2021-11-03 11:45:37 +0530

    ongoing committee done

--- pages/projects/ongoing/committee.php
+++ pages/projects/ongoing/committee.php
@@ -1,1 +1,13 @@
-Committee
+<link rel='stylesheet' href='../../../assets/styles/ongoing-projects-committee.css'/>
+<link rel='stylesheet' href='https://pro.fontawesome.com/releases/v5.10.0/css/all.css'
+      integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p' crossorigin='anonymous'/>
+
+<div class='card project-committee' id='committee-members'>
+    <div class='results'>
+        <div class='result2'>
+            <img src='../../../assets/images/user-default.png' height='100%' alt='user' class='coord-pic'/>
+            <div class='fname'>First Name</div>
+            <div class='lname'>Last Name</div>
+        </div>
+    </div>
+</div>
