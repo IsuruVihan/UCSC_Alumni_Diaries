0ffcf5a99f9ab81af05ea86c600c855c24078a8f
IsuruVihan <isuruvihan@gmail.com>
2021-11-05 17:00:49 +0530

    ongoing-projects assets-cash approvals done

--- pages/projects/ongoing/assets/cash/spend-approvals.php
+++ pages/projects/ongoing/assets/cash/spend-approvals.php
@@ -1,1 +1,20 @@
-Spend Approvals
+<link rel='stylesheet' href='../../../../../assets/styles/ongoing-projects-assets-cash-approvals.css'/>
+<link rel="stylesheet" href='https://pro.fontawesome.com/releases/v5.10.0/css/all.css'
+      integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p' crossorigin='anonymous'/>
+
+<table>
+    <tr>
+        <th class='h-1'>Amount (Rs.)</th>
+        <th class='h-2'>Description</th>
+        <th class='h-3'>Quotation</th>
+        <th class='h-4'>Status</th>
+        <th class='h-5'>Timestamp</th>
+    </tr>
+    <tr>
+        <td>600000</td>
+        <td>i3 8GB Dell Laptop * 4</td>
+        <td>Download</td>
+        <td>Approved</td>
+        <td>2021-10-08</td>
+    </tr>
+</table>
--- pages/projects/ongoing/committee.php
+++ pages/projects/ongoing/committee.php
@@ -3,6 +3,11 @@
       integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p' crossorigin='anonymous'/>
 
 <div class='card project-committee' id='committee-members'>
+    <div class='coord'>
+        <img src='../../../assets/images/user-default.png' height='25px' alt='user'/>
+        <div>First Name</div>
+        <div>Last Name</div>
+    </div>
     <div class='results'>
         <div class='result2'>
             <img src='../../../assets/images/user-default.png' height='100%' alt='user' class='coord-pic'/>
