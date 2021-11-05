3dd53a1d1751ab862c305eff0ef634b20ae5a779
IsuruVihan <isuruvihan@gmail.com>
2021-11-05 18:27:10 +0530

    ongoing-projects assets-cash spent-records done

--- pages/projects/ongoing/assets/cash/spent-records.php
+++ pages/projects/ongoing/assets/cash/spent-records.php
@@ -1,1 +1,18 @@
-Spent Records
+<link rel='stylesheet' href='../../../../../assets/styles/ongoing-projects-assets-cash-records.css'/>
+<link rel="stylesheet" href='https://pro.fontawesome.com/releases/v5.10.0/css/all.css'
+      integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p' crossorigin='anonymous'/>
+
+<table>
+    <tr>
+        <th class='h-1'>Amount (Rs.)</th>
+        <th class='h-2'>Description</th>
+        <th class='h-3'>Bill</th>
+        <th class='h-5'>Timestamp</th>
+    </tr>
+    <tr>
+        <td>600000</td>
+        <td>i3 8GB Dell Laptop * 4</td>
+        <td>Download</td>
+        <td>2021-10-08</td>
+    </tr>
+</table>
