d4d810c40a1088531f466308cc0fc2533b7ea393
jayaweera2 <subhagyajayaweera1998@gmail.com>
2021-11-05 07:36:52 +0530

    checkout test to check errors

--- components/header.php
+++ components/header.php
@@ -2,10 +2,14 @@
 <head>
 <title>UCSC Alumni Diaries</title>
 <link rel='stylesheet' href='../assets/styles/header.css' />
+<script
+    src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+    crossorigin="anonymous">
+</script>
 </head>
 <body>
 
-<!--Database connection-->
 <?php include('../db/db-conn.php'); ?>
 
 <nav class='navbar'>
@@ -18,12 +22,7 @@
         <li class='link'>
             <a href='../pages/home.php' class='anchor-tag'>Home</a>
         </li>
-        <li class='link'>
-            <a href='../pages/alumnus.php' class='anchor-tag'>Alumnus</a>
-        </li>
-        <li class='link'>
-            <a href='../pages/notifications.php' class='anchor-tag'>Notifications</a>
-        </li>
+       
         <li class='link'>
             <a href='../pages/projects.php' class='anchor-tag'>Projects</a>
         </li>
@@ -38,25 +37,19 @@
         </li>
         <li class='link'>
             <a href='../pages/suggestions.php' class='anchor-tag'>Suggestions</a>
-        </li>
-        <li class='link'>
-            <a href='../pages/wall.php' class='anchor-tag'>Wall</a>
-        </li>
-        <li class='link'>
-            <a href='../pages/chat.php' class='anchor-tag'>Chat</a>
-        </li>
-        <li class='link'>
-            <a href='../pages/admin.php' class='anchor-tag'>Admin</a>
-        </li>
-        <li class='link'>
-            <a href='../pages/my-account.php' class='anchor-tag'>My Account</a>
-        </li>
-        <li class='link'>
-            <a href='../pages/login.php' class='anchor-tag'>Login</a>
-        </li>
-        <li class='link'>
-            <a href='../pages/signup.php' class='anchor-tag'>Signup</a>
-        </li>
-        <li class='link'>Logout</li>
+        </li>  
+        <?php
+            if (!isset($_SESSION['Email'])) {
+                echo "
+                    <li class='link'>
+                        <a href='../pages/login.php' class='anchor-tag'>Login</a>
+                    </li>
+                    <li class='link'>
+                        <a href='../pages/signup.php' class='anchor-tag'>Signup</a>
+                    </li>
+                ";
+            }
+        ?>
+        
     </ul>
 </nav>
