016a3d44891735419cdad8829f6ed4d72ec942f0
IsuruVihan <isuruvihan@gmail.com>
2021-11-05 21:00:35 +0530

    ongoing-projects assets-items sketch done

---
+++ pages/projects/ongoing/assets/items/spend.php
@@ -0,0 +1,7 @@
+<link rel='stylesheet' href='../../../../../assets/styles/ongoing-projects-assets-items-spend.css'/>
+<link rel="stylesheet" href='https://pro.fontawesome.com/releases/v5.10.0/css/all.css'
+      integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p' crossorigin='anonymous'/>
+
+<form class='spend'>
+    Spend
+</form>
--- pages/projects/ongoing/committee.php
+++ pages/projects/ongoing/committee.php
@@ -3,12 +3,24 @@
       integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p' crossorigin='anonymous'/>
 
 <div class='card project-committee' id='committee-members'>
+    <div class='title'>Coordinator</div>
     <div class='coord'>
-        <img src='../../../assets/images/user-default.png' height='25px' alt='user'/>
+        <img src='../../../assets/images/user-default.png' height='100%' alt='user'/>
         <div>First Name</div>
         <div>Last Name</div>
     </div>
+    <div class='title'>Members</div>
     <div class='results'>
+        <div class='result2'>
+            <img src='../../../assets/images/user-default.png' height='100%' alt='user' class='coord-pic'/>
+            <div class='fname'>First Name</div>
+            <div class='lname'>Last Name</div>
+        </div>
+        <div class='result2'>
+            <img src='../../../assets/images/user-default.png' height='100%' alt='user' class='coord-pic'/>
+            <div class='fname'>First Name</div>
+            <div class='lname'>Last Name</div>
+        </div>
         <div class='result2'>
             <img src='../../../assets/images/user-default.png' height='100%' alt='user' class='coord-pic'/>
             <div class='fname'>First Name</div>
