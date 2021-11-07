b3ecd3d38f04c009210c646c47aae61834068ba9
Isuru Vihan Harischandra <isuruvihan@gmail.com>
2021-11-07 21:54:56 +0530

    ongoing assets-items done

--- pages/projects/ongoing/assets/items/spend.php
+++ pages/projects/ongoing/assets/items/spend.php
@@ -3,25 +3,33 @@
       integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p' crossorigin='anonymous'/>
 
 <form class='spend'>
-    <div class='group'>
-        <label for='id'>Item Id</label>
-        <select id='id' class='input-field'>
-            <option value='1'>1</option>
-            <option value='2'>2</option>
-            <option value='3'>3</option>
-        </select>
+    <div class='parent-group'>
+        <div class='group first'>
+            <label for='id'>Item Id</label>
+            <select id='id' class='input-field'>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+            </select>
+        </div>
+        <div class='group middle'>
+            <label for='name'>Item Name</label>
+            <input id='name' type='text' class='input-field' disabled/>
+        </div>
+        <div class='group last'>
+            <label for='available'>Available Quantity</label>
+            <input id='available' type='text' class='input-field' disabled/>
+        </div>
     </div>
-    <div class='group'>
-        <label for='name'>Item Name</label>
-        <input id='name' type='text' class='input-field' disabled/>
-    </div>
-    <div class='group'>
-        <label for='available'>Available Quantity</label>
-        <input id='available' type='text' class='input-field' disabled/>
-    </div>
-    <div class='group'>
-        <label for='description'>Description</label>
-        <input id='description' type='text' class='input-field'/>
+    <div class='parent-group'>
+        <div class='group first'>
+            <label for='spend'>Spend Quantity</label>
+            <input id='spend' type='text' class='input-field'/>
+        </div>
+        <div class='group last-2'>
+            <label for='description'>Description</label>
+            <input id='description' type='text' class='input-field'/>
+        </div>
     </div>
     <div class='group'>
         <label for='quotation'>Quotation attachment</label>
