61d66b9b02d24a3e12c243abc2def7e8f6b3995c
Shakya19020406 <shakya19020406@gmail.com>
2021-11-07 23:02:58 +0530

    server file done

--- pages/login.php
+++ pages/login.php
@@ -1,31 +1,28 @@
+<?php include('../server/session.php'); ?>
+
 <?php include '../components/header.php'; ?>
 
 <link rel='stylesheet' type='text/css' href='../assets/styles/login.css'>
 
 <script>
     $(document).ready(() => {
-        $('#login-cancel').click(() => {
-            $('#email, #password').removeClass('input-error, input-ok');
-            $('#email, #password').val('');
-        });
-
         $('#form').submit((event) => {
             event.preventDefault();
             let isComplete = true;
-
-            const login_email = $('#email').val();
-            const login_password = $('#password').val();
+            
+            const email = $('#email').val();
+            const password = $('#password').val();
+            const rememberMe = $('#remember-me').is(":checked");
 
             $('#email, #password').removeClass('input-error, input-ok');
 
-            if (login_email === '') {
+            if (email === '') {
                 $('#email').addClass('input-error');
                 isComplete = false;
             } else {
                 $('#email').addClass('input-ok');
             }
-
-            if (login_password === '') {
+            if (password === '') {
                 $('#password').addClass('input-error');
                 isComplete = false;
             } else {
@@ -33,66 +30,105 @@
             }
 
             if (isComplete) {
-                $('#email, #password').val('');
-                $('#email, #password').removeClass('input-error, input-ok');
+                $('#email, #password').val('').removeClass('input-error, input-ok');
             }
+            $('#flash-message').load("../server/login/login-submit.php", {
+                email: email,
+                password: password,
+                rememberMe: rememberMe
+            }, (response) => {
+                if (response === "1") {
+                    if (rememberMe) {
+                        if (!document.cookie.includes("Email")) {
+                            const d = new Date();
+                            d.setTime(d.getTime() + (31*24*60*60*1000));
 
-            $('#error-message').load('../server/login/login.php', {
-                login_email: login_email,
-                login_password: login_password,
+                            document.cookie = "Email=" + email + "; expires=" + d.toUTCString() + "; path=/";
+                            document.cookie = "Password=" + password + "; expires=" + d.toUTCString() + "; path=/";
+                        }
+                    } else {
+                        if (document.cookie.includes("Email")) {
+                            const d = new Date();
+                            d.setTime(d.getTime() - 1);
+
+                            document.cookie = "Email=; expires=" + d.toUTCString() + "; path=/";
+                            document.cookie = "Password=; expires=" + d.toUTCString() + "; path=/";
+                        }
+                    }
+                    window.location.replace("http://localhost/UCSC_Alumni_Diaries/pages/home.php");
+                }
             });
         });
-
     });
 </script>
 
 <div class='login'>
-    <div class='img'>
+    <div class=img>
         <!-- <img src='../assets/images/logo.jpeg' alt='logo' width='150' height='150'> <br>  -->
     </div>
     <h1 class='title'> Login </h1>
-    <form id='form' action='#' method="POST" class='login-form'>
+    <form id='form' class='login-form'>
         <div class='container1'>
             <div class='labels'>
                 <label>Email</label>
             </div>
-            <div class='input-field' id='email'>
-                <input type='text' placeholder='Enter Email'/>
-            </div>
+            <?php
+                if (isset($_COOKIE['Email'])) {
+                    echo "
+                        <input
+                            value='${_COOKIE['Email']}'
+                            class='input-field'
+                            id='email'
+                            type='text'
+                            name='email'
+                        />
+                    ";
+                } else {
+                    echo "<input class='input-field' id='email' type='text' name='email'/>";
+                }
+            ?>
         </div>
         <div class='container2'>
             <div class='labels'>
                 <label>Password</label>
             </div>
-            <div class='input-field' id='password'>
-                <input type='password' placeholder='Enter Password'/>
-            </div>
+            <?php
+                if (isset($_COOKIE['Password'])) {
+                    echo "
+                        <input
+                            value='${_COOKIE['Password']}'
+                            class='input-field'
+                            id='password'
+                            type='password'
+                            name='password'
+                        />
+                    ";
+                } else {
+                    echo "<input class='input-field' id='password' type='password' name='password'/>";
+                }
+            ?>
         </div>
-        <!-- <div id="error"></div> -->
-        <p id='error-message' class='error'> </p>
-        <div class='check-box' align='left' ;>
-            <input type='checkbox' id='checkbox'>
-            <label for="checkbox"> <b> Remember me </b> </label>
+        <div class='check-box'>
+            <?php
+                if (isset($_COOKIE['Password'])) {
+                    echo "<input type='checkbox' id='remember-me' checked>";
+                } else {
+                    echo "<input type='checkbox' id='remember-me'>";
+                }
+            ?>
+            <label for="remember-me"> <b> Remember me </b> </label>
         </div>
         <br />
-        <table class='logcan'>
-            <tr>
-                <td align='left'>
-                    <input id='login-submit' class='btn-solid-login' type='submit' name='' value=' Login  '>
-                </td>
-                <td align='right'>
-                    <input id='login-cancel' class='btn-solid-cancel' type='reset' name='' value='Cancel'>
-                </td>
-            </tr>
-        </table>
+        <div id='flash-message'></div>
         <br />
-        <center>
-            <a class='loginpagelink' href='forgot_password.php'> <b> Forgot Password? </b> </a> <br>
-            <p><b> Not one of us yet? </b> <a class='loginpagelink' href='signup.php'> <b> Sign Up </b> </a></p>
-        </center>
+        <div class='logcan'>
+            <input class='btn-solid-login' type='submit' value='LOGIN'>
+            <button class='btn-solid-cancel'>CANCEL</button>
+        </div>
+        <br />
+        <a class='loginpagelink' href='forgot_password.php'> <b> Forgot Password? </b> </a> <br>
+        <p><b> Not one of us yet? </b> <a class='loginpagelink' href='signup.php'> <b> Sign Up </b> </a></p>
     </form>
 </div>
-
-<script defer src='../js/login.js'></script>
 
 <?php include('../components/footer.php'); ?>
---
+++ server/login/login-submit.php
@@ -0,0 +1,48 @@
+<?php
+    include('../../db/db-conn.php');
+    
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $rememberMe = $_POST['rememberMe'];
+    
+    if (empty($email) || empty($password)) {
+        echo "<span class='message-error'>All fields are required</span>";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "<span class='message-error'>Email is not valid</span>";
+    } else {
+        $encPassword = md5($password);
+        $query = "SELECT * FROM registeredmembers WHERE Email='${email}' AND Password='${encPassword}'";
+        $result = mysqli_query($conn, $query);
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                session_start();
+                $_SESSION["Email"] = "${row['Email']}";
+                $_SESSION["FirstName"] = "${row['FirstName']}";
+                $_SESSION["LastName"] = "${row['LastName']}";
+                $_SESSION["NameWithInitials"] = "${row['NameWithInitials']}";
+                $_SESSION["Gender"] = "${row['Gender']}";
+                $_SESSION["Address"] = "${row['Address']}";
+                $_SESSION["NIC"] = "${row['NIC']}";
+                $_SESSION["ContactNumber"] = "${row['ContactNumber']}";
+                $_SESSION["Batch"] = "${row['Batch']}";
+                $_SESSION["IndexNumber"] = "${row['IndexNumber']}";
+                $_SESSION["PicSrc"] = "${row['PicSrc']}";
+                $_SESSION["Password"] = "${row['Password']}";
+                $_SESSION["AccType"] = "${row['AccType']}";
+                $_SESSION["SubscriptionType"] = "${row['SubscriptionType']}";
+                $_SESSION["SubscriptionDue"] = "${row['SubscriptionDue']}";
+                $_SESSION["Banned"] = false;
+    
+                $query2 = "SELECT * FROM bannedaccounts WHERE Email='${row['Email']}'";
+                $result2 = mysqli_query($conn, $query2);
+                if (mysqli_num_rows($result2) > 0) {
+                    $_SESSION["Banned"] = true;
+                }
+                echo "1";
+            }
+        } else {
+            echo "<span class='message-error'>Email and Password not matching</span>";
+        }
+    }
+    
+    
