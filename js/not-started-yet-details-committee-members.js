ddfbd84cbd6cb1800bbc11f60affa1e62d00a036
Sahani19020821 <sahani19020821@gmail.com>
2021-11-10 01:54:39 +0530

    Removed iframes of Not started yet projects

--- js/not-started-yet-details-committee-members.js
+++
@@ -1,8 +0,0 @@
-const ClickLink = (id) => {
-    document.getElementById(id).classList.add('clicked-link');
-    if (id==='l-1') {
-        document.getElementById('l-2').classList.remove('clicked-link');
-    } else if (id==='l-2') {
-        document.getElementById('l-1').classList.remove('clicked-link');
-    }
-}
