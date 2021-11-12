a0b727cfdb98af056dfce71efd509858a56bc47b
Shakya19020406 <shakya19020406@gmail.com>
2021-11-12 18:06:39 +0530

    suggestions backend ammended

--- pages/suggestions.php
+++ pages/suggestions.php
@@ -1,9 +1,26 @@
-<link rel='stylesheet' href='../assets/styles/suggestions.css'/>
-<link rel="stylesheet" href='https://pro.fontawesome.com/releases/v5.10.0/css/all.css'
-      integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p' crossorigin='anonymous'/>
+<?php include('../server/session.php'); ?>
+
+    <link rel='stylesheet' href='../assets/styles/suggestions.css'/>
+    <link rel="stylesheet" href='https://pro.fontawesome.com/releases/v5.10.0/css/all.css'
+          integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p' crossorigin='anonymous'/>
 
 <?php include('../components/header.php'); ?>
 
+<?php include('../server/suggestions/submit-suggestion.php'); ?>
+
+<script>
+    $(document).ready(() => {
+        $('#all-suggestions').load("../server/suggestions/show-suggestions.php");
+    });
+    const DeleteSuggestion = (id) => {
+        $('#flash-message').load("../server/suggestions/delete-suggestion.php", {
+            id: id
+        });
+        setTimeout(() => window.history.go(), 1);
+    }
+</script>
+
+<div id='flash-message' class='flash-message'></div>
 <div class='main-container'>
     <p class='breadcrumb'>
         <a href='home.php'>Home</a> / Suggestions
@@ -12,148 +29,83 @@
         <i class="fas fa-lightbulb"></i> Suggestions
     </p>
 </div>
-<div class='main-box'>
-    <div class='suggestions-form'>
-        <div class='card suggestions-box'>
-            <div class='title'>
-                Any Suggestions?
-            </div>
-            <div class='fields'>
-                <form action='suggestions.php'>
-                    <label class='suggestion-form-label'> Full Name </label>
-                    <input type='text' class='full-name details-field' placeholder='Your Name'>
-                    <label class='suggestion-form-label'> Email </label>
-                    <input type='text' class='email-field details-field' placeholder='Email'>
-                    <label class='suggestion-form-label'> Title </label>
-                    <input type='text' class='title-field details-field' placeholder='Title'>
-                    <input type='file' class='image-field' name='suggestion-image' accept='image/*'>
-                    <textarea class='comment'>    Enter your suggestions here... </textarea>
-                    <div class='submit-button'>
-                        <input type='submit' class='submit-btn' value='Submit'>
-                    </div>
-                </form>
-            </div>
-        </div>
+<div class='suggestions'>
+    <?php
+        if (isset($_SESSION['Email'])) {
+            if ($_SESSION['AccType']=='Member') {
+                echo "
+                    <form
+                        action='../server/suggestions/submit-suggestion.php'
+                        method='post'
+                        id='suggestion-form'
+                        class='submit-suggestion-form'
+                        enctype='multipart/form-data'
+                    >
+                        <div class='section'>
+                            <label for='user-name'>Your Name</label><br/>
+                            <input class='input-field' type='text' id='user-name' name='user-name'>
+                        </div>
+                        <div class='section'>
+                            <label for='user-email'>Your Email</label><br/>
+                            <input class='input-field' type='text' id='user-email' name='user-email'>
+                        </div>
+                        <div class='section'>
+                            <label for='suggestion-title'>Title</label><br/>
+                            <input class='input-field' type='text' id='suggestion-title' name='suggestion-title'>
+                        </div>
+                            <div class='section'>
+                                <label for='suggestion-content'>Suggestion</label><br/>
+                                <textarea class='input-field text-area' id='suggestion-content' name='suggestion-content'></textarea>
+                            </div>
+                            <input type='file' id='image-attach' name='image-attach'>
+                            <div class='section-2'>
+                                <input id='submit-btn' name='submit-btn' class='btn accept-btn' type='submit'>
+                                <button class='btn remove-btn' type='reset'>Cancel</button>
+                            </div>
+                        </form>
+                    ";
+                } elseif ($_SESSION['AccType']=='TopBoard') {
+                    echo "
+                        <div class='all-suggestions' id='all-suggestions'>
+                        
+                        
+                        </div>
+                    ";
+                }
+            } else {
+                echo "
+                    <form
+                        action='../server/suggestions/submit-suggestion.php'
+                        method='post'
+                        id='suggestion-form'
+                        class='submit-suggestion-form'
+                        enctype='multipart/form-data'
+                    >
+                        <div class='section'>
+                            <label for='user-name'>Your Name</label><br/>
+                            <input class='input-field' type='text' id='user-name' name='user-name'>
+                        </div>
+                        <div class='section'>
+                            <label for='user-email'>Your Email</label><br/>
+                            <input class='input-field' type='text' id='user-email' name='user-email'>
+                        </div>
+                        <div class='section'>
+                            <label for='suggestion-title'>Title</label><br/>
+                            <input class='input-field' type='text' id='suggestion-title' name='suggestion-title'>
+                        </div>
+                        <div class='section'>
+                            <label for='suggestion-content'>Suggestion</label><br/>
+                            <textarea class='input-field text-area' id='suggestion-content' name='suggestion-content'></textarea>
+                        </div>
+                        <input type='file' id='image-attach' name='image-attach'>
+                        <div class='section-2'>
+                            <input id='submit-btn' name='submit-btn' class='btn accept-btn' type='submit'>
+                            <button class='btn remove-btn' type='reset'>Cancel</button>
+                        </div>
+                    </form>
+                ";
+            }
+        ?>
     </div>
-    <div class='suggestions-list-item'>
-        <div class='card suggestions-box'>
-            <div class='title'>
-                Suggestions List
-            </div>
-            <div class='flexbox-container'>
-                <div class='flexbox-item'>
-                    <div class='suggests'>
-                        <div class='pro-pic'>
-                            <div class='tandp'>
-                                <img class='img' src='../assets/images/user-default.png' width='100%' height=''
-                                     class='user-pic' alt='user-pic'/>
-                            </div>
-                        </div>
-                        <div class='ntt'>
-                            <div class='titlesl list-details'> Title</div>
-                            <div class='fullname list-details'> Full Name</div>
-                            <div class='timestamp list-details'> Time</div>
-                        </div>
-                    </div>
-                    <div class='sugestedcontent'>
-                        <div class='attachment'> Attachment</div>
-                        <div class='description'> Suggestion</div>
-                    </div>
-                    <div class='deletecontainer'>
-                        <input type='reset' class='delete-btn' value='Delete'>
-                    </div>
-                </div>
-                <div class='flexbox-item'>
-                    <div class='suggests'>
-                        <div class='pro-pic'>
-                            <div class='tandp'>
-                                <img class='img' src='../assets/images/user-default.png' width='100%' height=''
-                                     class='user-pic' alt='user-pic'/>
-                            </div>
-                        </div>
-                        <div class='ntt'>
-                            <div class='titlesl list-details'> Title</div>
-                            <div class='fullname list-details'> Full Name</div>
-                            <div class='timestamp list-details'> Time</div>
-                        </div>
-                    </div>
-                    <div class='sugestedcontent'>
-                        <div class='attachment'> Attachment</div>
-                        <div class='description'> Suggestion</div>
-                    </div>
-                    <div class='deletecontainer'>
-                        <input type='reset' class='delete-btn' value='Delete'>
-                    </div>
-                </div>
-                <div class='flexbox-item'>
-                    <div class='suggests'>
-                        <div class='pro-pic'>
-                            <div class='tandp'>
-                                <img class='img' src='../assets/images/user-default.png' width='100%' height=''
-                                     class='user-pic' alt='user-pic'/>
-                            </div>
-                        </div>
-                        <div class='ntt'>
-                            <div class='titlesl list-details'> Title</div>
-                            <div class='fullname list-details'> Full Name</div>
-                            <div class='timestamp list-details'> Time</div>
-                        </div>
-                    </div>
-                    <div class='sugestedcontent'>
-                        <div class='attachment'> Attachment</div>
-                        <div class='description'> Suggestion</div>
-                    </div>
-                    <div class='deletecontainer'>
-                        <input type='reset' class='delete-btn' value='Delete'>
-                    </div>
-                </div>
-                <div class='flexbox-item'>
-                    <div class='suggests'>
-                        <div class='pro-pic'>
-                            <div class='tandp'>
-                                <img class='img' src='../assets/images/user-default.png' width='100%' height=''
-                                     class='user-pic' alt='user-pic'/>
-                            </div>
-                        </div>
-                        <div class='ntt'>
-                            <div class='titlesl list-details'> Title</div>
-                            <div class='fullname list-details'> Full Name</div>
-                            <div class='timestamp list-details'> Time</div>
-                        </div>
-                    </div>
-                    <div class='sugestedcontent'>
-                        <div class='attachment'> Attachment</div>
-                        <div class='description'> Suggestion</div>
-                    </div>
-                    <div class='deletecontainer'>
-                        <input type='reset' class='delete-btn' value='Delete'>
-                    </div>
-                </div>
-                <div class='flexbox-item'>
-                    <div class='suggests'>
-                        <div class='pro-pic'>
-                            <div class='tandp'>
-                                <img class='img' src='../assets/images/user-default.png' width='100%' height=''
-                                     class='user-pic' alt='user-pic'/>
-                            </div>
-                        </div>
-                        <div class='ntt'>
-                            <div class='titlesl list-details'> Title</div>
-                            <div class='fullname list-details'> Full Name</div>
-                            <div class='timestamp list-details'> Time</div>
-                        </div>
-                    </div>
-                    <div class='sugestedcontent'>
-                        <div class='attachment'> Attachment</div>
-                        <div class='description'> Suggestion</div>
-                    </div>
-                    <div class='deletecontainer'>
-                        <input type='reset' class='delete-btn' value='Delete'>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 
 <?php include('../components/footer.php'); ?>
