631e3c95b3917c040a98aa76fb2607bf0cdcdbd9
Sahani19020821 <sahani19020821@gmail.com>
2021-11-20 09:52:10 +0530

    My Account Change Password Section done

---
+++ pages/my-account/change-password-success-msg.php
@@ -0,0 +1,13 @@
+<?php include('../../server/session.php'); ?>
+<link rel='stylesheet' href='../../assets/styles/my-account.css'/>
+<link rel="stylesheet" href='https://pro.fontawesome.com/releases/v5.10.0/css/all.css'
+      integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p' crossorigin='anonymous'/>
+<script
+    src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+    crossorigin="anonymous">
+</script>
+
+<div class='success-msg'>
+    <p>Password has been changed successfully!</p>
+</div>
