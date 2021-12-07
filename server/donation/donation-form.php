b647fa0705e085d9d821aef672ba6c8139aa7122
jayaweera#2 <subhagyajayaweera1998@gmail.com>
2021-12-07 11:23:37 +0530

    Complete donation backend

--- server/donation/donation-form.php
+++ server/donation/donation-form.php
@@ -1,37 +1,53 @@
-<?php 
-  include('../../db/db-conn.php');
+<?php
 
-//   $donorname = $_POST['donorname'];
-//   $donormail = $_POST['donormail'];
-//   $donoramount = $_POST['donoramount'];
-//   $donorfile = $_FILES['donorfile'];
+include('../../db/db-conn.php');
 
-$targetPath = "'../../uploads/donations/'" . basename($_FILES['donorfile']['name']);
-move_uploaded_file($_FILES['donorfile']['tmp_name'], $targetPath);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_FILES['files'])) {
+        $errors = [];
+        $path = '../../uploads/donation/';
+        $extensions = ['jpg', 'jpeg', 'png', 'pdf'];
 
-  if (
-      empty($donor_name)|| empty($donor_mail)||empty($donor_amount)
-  ) {
-      echo "<span class='message-error'> All the fields are required</span>";
-  }elseif(!filter_var($donor_mail,FILTER_VALIDATE_EMAIL)){
-      echo"<span class='message-error'>Email is not valid</span>";
-      
-  }else{
-      $query = "INSERT INTO cashdonations(
-                DonorName,
-                DonorEmail,
-                Amount
-             )VALUES(
-                '$donor_name',
-                '$donor_mail',
-                '$donor_amount'
-             )";
-        if(mysqli_query($conn,$query)){
-            echo"<span class='message-success'>Donation has been sucessfully submitted</span>";
+        $all_files = count($_FILES['files']['tmp_name']);
 
-        }else{
-            echo"<span class='message-error'>Server Error: ". mysqli_error($conn) . "</span";
-        }    
+        for ($i = 0; $i < $all_files; $i++) {
+            $file_name = $_FILES['files']['name'][$i];
+            $file_tmp = $_FILES['files']['tmp_name'][$i];
+            $file_type = $_FILES['files']['type'][$i];
+            $file_size = $_FILES['files']['size'][$i];
+            $file_ext = strtolower(end(explode('.', $_FILES['files']['name'][$i])));
+
+            $fileNameNew = uniqid('', true) . "." . $file_ext;
+            $file = $path . $fileNameNew;
+
+            if (!in_array($file_ext, $extensions)) {
+                $errors[] = 'Extension not allowed: ' . $file_name . ' ' . $file_type;
+            }
+
+            if ($file_size > 2097152) {
+                $errors[] = 'File size exceeds limit: ' . $file_name . ' ' . $file_type;
+            }
+
+            if (empty($errors)) {
+                move_uploaded_file($file_tmp, $file);
+            }
+        }
+        if ($errors) print_r($errors);
     }
 
-?>
+    $donor_name = $_POST['donor-name'];
+    $donor_email = $_POST['donor-mail'];
+    $donor_amount = $_POST['donor-amount'];
+    
+    if (($donor_amount > 0) && filter_var($donor_email, FILTER_VALIDATE_EMAIL)){
+        if (!empty($donor_name) && !empty($donor_email) && !empty($donor_amount) && empty($file_name)) {
+            $query = "INSERT INTO cashdonations (DonorName, DonorEmail, DonationFor, Amount) VALUES ('$donor_name','$donor_email','Association','$donor_amount')";
+            $result = mysqli_query($conn, $query);
+        }
+        if (!empty($donor_name) && !empty($donor_email)  && !empty($donor_amount) && !empty($file_name)) {
+            $query = "INSERT INTO cashdonations (DonorName, DonorEmail, DonationFor, PayslipSrc, Amount) VALUES ('$donor_name','$donor_email','Association','$file','$donor_amount')";
+            $result = mysqli_query($conn, $query);
+        }
+    }
+    
+}
