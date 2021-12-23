bb9c1bc383ad83cfd55f3171b312d8347ecb3a73
Isuru Vihan Harischandra <isuruvihan@gmail.com>
2021-12-23 13:47:22 +0530

    Closed projects done

--- pages/closed-projects.php
+++ pages/closed-projects.php
@@ -6,154 +6,80 @@
 
 <?php include('../components/header.php'); ?>
 
-<div class='main-container'>
-    <p class='breadcrumb'>
-        <a href='home.php'>Home</a> /
-        <a href='projects.php'>Projects</a> / Closed
-    </p>
-    <p class='main-title'>
-        <i class="fas fa-times-circle"></i> Closed
-    </p>
-</div>
-<div class='closed-projects'>
-    <div class='card projects-list'>
-        <div class='title'>
-            Related Closed Projects
-        </div>
-        <div class='filter'>
-            <div class='col1'>
-                <input class='input-field date-field' type='date' placeholder='First Name'/>
-                to
-                <input class='input-field date-field' type='date' placeholder='Last Name'/>
-            </div>
-            <div class='col2'>
-                <label>My Projects</label>
-                <input class='input-field' type='checkbox'>
-            </div>
-            <div class='col3'>
-                <button class='filter-btn btn'>Filter</button>
-            </div>
-        </div>
-        <div class='results'>
-            <div class='result' onmouseover="DisplayButtons('p-list-1')" onmouseout="HideButtons('p-list-1')">
-                <p class='request-id'>ProjectId</p>
-                <p class='request-id'>ProjectName</p>
-                <div class='buttons' id='p-list-1'>
-                    <button class='view-btn btn'>View</button>
-                </div>
-            </div>
-            <div class='result' onmouseover="DisplayButtons('p-list-2')" onmouseout="HideButtons('p-list-2')">
-                <p class='request-id'>ProjectId</p>
-                <p class='request-id'>ProjectName</p>
-                <div class='buttons' id='p-list-2'>
-                    <button class='view-btn btn'>View</button>
-                </div>
-            </div>
-            <div class='result' onmouseover="DisplayButtons('p-list-3')" onmouseout="HideButtons('p-list-3')">
-                <p class='request-id'>ProjectId</p>
-                <p class='request-id'>ProjectName</p>
-                <div class='buttons' id='p-list-3'>
-                    <button class='view-btn btn'>View</button>
-                </div>
-            </div>
-            <div class='result' onmouseover="DisplayButtons('p-list-4')" onmouseout="HideButtons('p-list-4')">
-                <p class='request-id'>ProjectId</p>
-                <p class='request-id'>ProjectName</p>
-                <div class='buttons' id='p-list-4'>
-                    <button class='view-btn btn'>View</button>
-                </div>
-            </div>
-            <div class='result' onmouseover="DisplayButtons('p-list-5')" onmouseout="HideButtons('p-list-5')">
-                <p class='request-id'>ProjectId</p>
-                <p class='request-id'>ProjectName</p>
-                <div class='buttons' id='p-list-5'>
-                    <button class='view-btn btn'>View</button>
-                </div>
-            </div>
-            <div class='result' onmouseover="DisplayButtons('p-list-6')" onmouseout="HideButtons('p-list-6')">
-                <p class='request-id'>ProjectId</p>
-                <p class='request-id'>ProjectName</p>
-                <div class='buttons' id='p-list-6'>
-                    <button class='view-btn btn'>View</button>
-                </div>
-            </div>
-            <div class='result' onmouseover="DisplayButtons('p-list-7')" onmouseout="HideButtons('p-list-7')">
-                <p class='request-id'>ProjectId</p>
-                <p class='request-id'>ProjectName</p>
-                <div class='buttons' id='p-list-7'>
-                    <button class='view-btn btn'>View</button>
-                </div>
-            </div>
-            <div class='result' onmouseover="DisplayButtons('p-list-8')" onmouseout="HideButtons('p-list-8')">
-                <p class='request-id'>ProjectId</p>
-                <p class='request-id'>ProjectName</p>
-                <div class='buttons' id='p-list-8'>
-                    <button class='view-btn btn'>View</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class='card project-details'>
-        <div class='title project-name-div' id='project-name-div'>
-            Project Name
-        </div>
-        <div class='project-status'>
-            Started on
-        </div>
-        <div class='project-status'>
-            Closed on
-        </div>
-        <div class='project-description' id='project-description'>
-            Project description comes here...
-        </div>
-        <div class='coord'>
-            <img src='../assets/images/user-default.png' height='100%' alt='user' class='coord-pic'/>
-            <p class='coord-fname'>First Name</p>
-            <p class='coord-lname'>Last Name</p>
-        </div>
-    </div>
-    <div class='card project-committee' id='committee-members'>
-        <div class='title'>
-            Committee Members
-        </div>
-        <div class='results'>
-            <div class='result'>
-                <img src='../assets/images/user-default.png' height='100%' alt='user' class='coord-pic'/>
-                <div class='fname'>First Name</div>
-                <div class='lname'>Last Name</div>
-            </div>
-            <div class='result'>
-                <img src='../assets/images/user-default.png' height='100%' alt='user' class='coord-pic'/>
-                <div class='fname'>First Name</div>
-                <div class='lname'>Last Name</div>
-            </div>
-            <div class='result'>
-                <img src='../assets/images/user-default.png' height='100%' alt='user' class='coord-pic'/>
-                <div class='fname'>First Name</div>
-                <div class='lname'>Last Name</div>
-            </div>
-            <div class='result'>
-                <img src='../assets/images/user-default.png' height='100%' alt='user' class='coord-pic'/>
-                <div class='fname'>First Name</div>
-                <div class='lname'>Last Name</div>
-            </div>
-            <div class='result'>
-                <img src='../assets/images/user-default.png' height='100%' alt='user' class='coord-pic'/>
-                <div class='fname'>First Name</div>
-                <div class='lname'>Last Name</div>
-            </div>
-        </div>
-    </div>
-    <div class='card expenditures'>
-        <div class='title'>
-            Expenditures
-        </div>
-        <div class='gen-report-div'>
-            <button class='btn gen-rep-btn'>Expenditure Report</button>
-        </div>
-    </div>
-</div>
+<script>
+    $(document).ready(() => {
+        $('#closed-projects-list').load("../server/projects/closed/render-list.php");
+        $('#closed-projects-filter').submit((event) => {
+            event.preventDefault();
 
-<script src='../js/closed-projects.js'></script>
+            const Date1 = $('#closed-date1').val();
+            const Date2 = $('#closed-date2').val();
+            const Id = $('#closed-id').val();
+            const Name = $('#closed-name').val();
+            const My = $('#closed-my').is(":checked");
+
+            $('#closed-projects-list').load("../server/projects/closed/filter.php", {
+                Date1: Date1,
+                Date2: Date2,
+                Id: Id,
+                Name: Name,
+                My: My
+            });
+        });
+    });
+
+    const ViewAllProjectDetails = (id) => {
+        $('#closed-project-details').load("../server/projects/closed/view-details.php", {
+            ProjectId: id
+        });
+        $('#closed-project-committee-members').load("../server/projects/closed/view-committee-members.php", {
+            ProjectId: id
+        });
+        $('#closed-project-expenditures').load("../server/projects/closed/view-expenditures.php", {
+            ProjectId: id
+        });
+    }
+</script>
+
+    <div class='main-container'>
+        <p class='breadcrumb'>
+            <a href='home.php'>Home</a> /
+            <a href='projects.php'>Projects</a> / Closed
+        </p>
+        <p class='main-title'>
+            <i class="fas fa-check-circle"></i> Closed
+        </p>
+    </div>
+    <div class='closed-projects'>
+        <div class='card projects-list'>
+            <div class='title'>
+                Related Closed Projects
+            </div>
+            <form id='closed-projects-filter' class='filter'>
+                <div class='col1'>
+                    <input class='input-field date-field' type='date' id='closed-date1'/>
+                    to
+                    <input class='input-field date-field' type='date' id='closed-date2'/>
+                </div>
+                <div class='col1'>
+                    <input class='input-field date-field' type='text' placeholder='Project Name'  id='closed-name'/>
+                    <input class='input-field date-field' type='text' placeholder='Project Id'  id='closed-id'/>
+                </div>
+                <div class='col2'>
+                    <label>My Projects</label>
+                    <input class='input-field' type='checkbox' id='closed-my'/>
+                </div>
+                <div class='col4'>
+                    <input type='submit' class='filter-btn btn' value='Filter'/>
+                </div>
+            </form>
+            <div class='results' id='closed-projects-list'></div>
+        </div>
+        <div class='card project-details' id='closed-project-details'></div>
+        <div class='card project-committee' id='closed-project-committee-members'></div>
+        <div class='card expenditures' id='closed-project-expenditures'></div>
+    </div>
+
+    <script src='../js/closed-projects.js'></script>
 
 <?php include('../components/footer.php'); ?>
