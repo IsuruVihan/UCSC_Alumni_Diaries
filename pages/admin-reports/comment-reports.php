8ed4e7d0d3d0dcc55b9c65e0851e1259875de689
Isuru Vihan Harischandra <isuruvihan@gmail.com>
2022-02-17 09:37:03 +0530

    Removed test data

--- pages/admin-reports/comment-reports.php
+++ pages/admin-reports/comment-reports.php
@@ -32,63 +32,7 @@
                 Comment Reports
             </div> 
             <div class='flexbox-container' id='crepo'>
-                <!-- <div class='flexbox-item'>
-                    <div class='rdetails'>
-                        <div class='propic'>
-                            <img class='img' src='../../assets/images/user-default.png' width='100%' height=''
-                                 class='user-pic' alt='user-pic'/>
-                        </div>
-                        <div class='c-reporter'>
-                            <div class='c-reporter-name'>
-                                <div class='cfname'>
-                                    <label class='alllabels'> Reporter Name </label>
-                                    <div class='cfirst-name list-details'> First Name (Reporter)
-                                    </div>
-                                </div>
-                                <div class='clname'>
-                                    <div class='clast-name list-details'> Last Name (Reporter)
-                                    </div>
-                                </div>
-                            </div> 
-                            <div class='c-report-comment'>
-                                <div class='cid'>
-                                    <label class='alllabels'> Repoted Post ID </label>
-                                    <div class='cpost-id list-details'> Reported Post ID
-                                    </div>
-                                </div>
-                                <div class='cowner'>
-                                    <label class='alllabels'> Post Owner </label>
-                                    <div class='cpost-owner list-details'> Post Owner
-                                    </div>
-                                </div>
-                            </div> 
-                            <div class='commentdetails'>
-                                <div class='rcid'>
-                                    <label class='alllabels'> Reported Comment ID </label>
-                                    <div class='cpost-id list-details'> Reported Comment ID
-                                    </div>
-                                </div>
-                                <div class='rcowner'>
-                                    <label class='alllabels'> Comment Owner </label>
-                                    <div class='cpost-id list-details'> Comment Owner
-                                    </div>
-                                </div>
-                            </div> 
-                            <div class='rcause'>
-                                <div class='report-cause'>
-                                    <label class='alllabels'> Cause for Reporting </label>
-                                    <div class='thee-cause list-details'> Cause for Reporting
-                                    </div>
-                                </div>
-                            </div>
-                        </div> 
-                    </div> 
-                    <div class='rdelete'>
-                        <input type='reset' class='delete-btn' value='Delete'>
-                    </div>
-                </div>  -->
-                
-            </div> <!-- flexbox container -->
-        </div> <!-- card reports -->
-    </div> <!-- comment-reports -->
-</div> <!-- main box -->
+            </div>
+        </div>
+    </div>
+</div>
